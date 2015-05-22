0d4d25127f5332089153178455f52be26fa28746
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2015-05-22 02:33:04 +0100

    Add tester pcb svgs

--- gulpfile.coffee
+++ gulpfile.coffee
@@ -1,15 +1,21 @@
-gulp = require("gulp");
+gulp = require("gulp")
 elm  = require("gulp-elm")
 
-gulp.task "elm-init", elm.init
+paths = {elm : ["src/*.elm"], html: ["src/*.html"]}
+
+gulp.task("elm-init", elm.init)
 
 gulp.task "elm", ["elm-init"], () ->
-    return gulp.src("src/*.elm")
+    return gulp.src(paths.elm)
         .pipe(elm())
         .pipe(gulp.dest("build/"))
 
 gulp.task "html", () ->
-    return gulp.src("src/*.html")
+    return gulp.src(paths.html)
         .pipe(gulp.dest("build/"));
 
 gulp.task("default", ["elm", "html"])
+
+gulp.task "watch", () ->
+    gulp.watch(paths.elm, ["elm"])
+    gulp.watch(paths.html, ["html"])
--- src/main.elm
+++ src/main.elm
@@ -1,3 +1,31 @@
 import Graphics.Element exposing (..)
+import Signal exposing (..)
+import Window
 
-main = show "hi"
+boardImage side w =
+    let aspect = 37 / 60
+    --let aspect = 2.1 / 2.7
+    in image
+        w
+        (round (toFloat w * aspect))
+        ("../pcbs/bus-pirate-v3.6/svg/"
+            ++ side ++ ".svg")
+
+
+boardView w =
+    let boardW        = (w - (spacerW*3)) // 2
+        spacerW       = w // 16
+        maxH          = 400
+        frontExpanded = boardImage "front" boardW
+        boardH        = heightOf frontExpanded
+    in flow right
+        [ spacer spacerW 1
+        , if boardH < maxH then frontExpanded else height maxH frontExpanded
+        , spacer spacerW 1
+        , boardImage "back" boardW
+        ]
+
+view (w,h) = flow down [spacer 1 64, boardView w]
+
+main : Signal Element
+main = map view Window.dimensions
