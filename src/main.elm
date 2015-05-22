f0181eb98648649f375fcf1e550937dfcb708a9d
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2015-05-22 02:51:40 +0100

    Give up on pre-deterimined svg aspect

--- src/main.elm
+++ src/main.elm
@@ -2,30 +2,29 @@ import Graphics.Element exposing (..)
 import Signal exposing (..)
 import Window
 
-boardImage side w =
-    let aspect = 37 / 60
-    --let aspect = 2.1 / 2.7
-    in image
-        w
-        (round (toFloat w * aspect))
-        ("../pcbs/bus-pirate-v3.6/svg/"
-            ++ side ++ ".svg")
+boardImage side w h =
+    image w h
+     ("../pcbs/bus-pirate-v3.6/svg/"
+         ++ side ++ ".svg")
+
+--boardImage side w h =
+--    image w h
+--     ("../pcbs/arduino-uno/svg/"
+--         ++ side ++ ".svg")
 
 
-boardView w =
-    let boardW        = (w - (spacerW*3)) // 2
-        spacerW       = w // 16
-        maxH          = 400
-        frontExpanded = boardImage "front" boardW
-        boardH        = heightOf frontExpanded
+boardView w h =
+    let boardW  = (w - (spacerW*3)) // 2
+        spacerW = w // 16
+        maxH    = max 300 (round (toFloat h / 3.3))
     in flow right
         [ spacer spacerW 1
-        , if boardH < maxH then frontExpanded else height maxH frontExpanded
+        , boardImage "front" boardW maxH
         , spacer spacerW 1
-        , boardImage "back" boardW
+        , boardImage "back" boardW maxH
         ]
 
-view (w,h) = flow down [spacer 1 64, boardView w]
+view (w,h) = flow down [spacer 1 64, boardView w h]
 
 main : Signal Element
 main = map view Window.dimensions
