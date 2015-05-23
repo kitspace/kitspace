e2b67a8ab7827ca1f24ba692d370ee31b4f0c490
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2015-05-23 14:16:08 +0100

    Tweak svg placement

--- src/main.elm
+++ src/main.elm
@@ -2,21 +2,21 @@ import Graphics.Element exposing (..)
 import Signal exposing (..)
 import Window
 
-boardImage side w h =
-    image w h
-     ("../pcbs/bus-pirate-v3.6/svg/"
-         ++ side ++ ".svg")
-
 --boardImage side w h =
 --    image w h
---     ("../pcbs/arduino-uno/svg/"
+--     ("../pcbs/bus-pirate-v3.6/svg/"
 --         ++ side ++ ".svg")
+
+boardImage side w h =
+    image w h
+     ("../pcbs/arduino-uno/svg/"
+         ++ side ++ ".svg")
 
 
 boardView w h =
     let boardW  = (w - (spacerW*3)) // 2
         spacerW = w // 16
-        maxH    = max 300 (round (toFloat h / 3.3))
+        maxH    = max 100 (round (toFloat h / 3.3))
     in flow right
         [ spacer spacerW 1
         , boardImage "front" boardW maxH
@@ -24,7 +24,7 @@ boardView w h =
         , boardImage "back" boardW maxH
         ]
 
-view (w,h) = flow down [spacer 1 64, boardView w h]
+view (w,h) = flow down [spacer 1 10, boardView w h]
 
 main : Signal Element
 main = map view Window.dimensions
