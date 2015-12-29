c8ebbd3669f4a96e288b5e6a5e7131eab4e148ef
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2015-12-29 17:12:29 +0000

    Add deploy script for travis

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
+    exit 0;
+fi
+
+set -e
+
+cd build
+
+git init
+echo "kitnic.it" > CNAME
+git config user.name "Travis CI"
+git config user.email "travisCI@monostable.co.uk"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/monostable/kitnic" master:gh-pages > /dev/null 2>&1
