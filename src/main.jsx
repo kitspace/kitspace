749ebd1583eccfeb5a0fabdcb5f9b93194e55b62
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-01-03 22:37:25 +0000

    Refactor and tweak main

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,9 @@ var TitleBar = React.createClass({
         , boxShadow: '0px 0.1em 0.5em #000'
         }
       }>
+      <a href='/'>
         <img src='images/logo.png' style={{padding: '10 10 10 10'}}/>
+        </a>
       </div>
     );
   }
@@ -34,15 +36,12 @@ var BoardThumb = React.createClass({
   handleMouseOut: function (e) {
     this.setState({hover: false});
   },
-  handleClick: function (e) {
-    document.location.href += 'boards/' + this.props.data.id;
-  },
   render: function () {
     var style = {
       backgroundColor: '#DFDFDF'
       , width: (dim.thumb.w * 0.8) + 32 + 16
       , height: (dim.thumb.h * 0.8) + dim.thumb.capH + 16 + 5
-      , borderRadius: 5
+      , borderRadius: 10
       , color: 'rgb(55,55,55)'
       , marginTop:  16
       , marginRight: 0
@@ -50,7 +49,6 @@ var BoardThumb = React.createClass({
       , marginLeft: 16
       , float: 'left'
       , border: '1px solid #CFCFCF'
-      , cursor: 'pointer'
     };
     var imgStyle = {
       width: dim.thumb.w * 0.8
@@ -80,30 +78,31 @@ var BoardThumb = React.createClass({
     };
 
     if (this.state.hover) {
-      style.backgroundColor = '#F0F0F0';
-      style.height += (dim.thumb.h * 0.2) + 80;
-      style.width += (dim.thumb.w * 0.2);
-      style.marginBottom -= (dim.thumb.h * 0.2) + 60;
-      style.marginTop -= 20;
-      style.marginLeft -= (dim.thumb.h * 0.13);
-      style.marginRight -= (dim.thumb.h * 0.13);
-      style.opacity = 0.93;
-      imgStyle.opacity = 1.0;
-      imgStyle.width = dim.thumb.w;
-      imgStyle.height = dim.thumb.h;
-      titleStyle.height -= 24;
-      imgStyle.padding = '26px 27px 0px 27px';
-      style.width += 20;
-      style.position = 'relative';
-      descrStyle.visibility =  'visible';
-      style.boxShadow =  '0px 0.1em 0.5em #000';
+      style.backgroundColor  = '#F0F0F0';
+      style.height          += (dim.thumb.h * 0.2) + 80;
+      style.width           += (dim.thumb.w * 0.2);
+      style.marginBottom    -= (dim.thumb.h * 0.2) + 60;
+      style.marginTop       -= 20;
+      //this is what works to keep the adjacent from moving
+      style.marginLeft      -= (dim.thumb.h * 0.177);
+      style.marginRight     -= (dim.thumb.h * 0.177);
+      style.opacity          = 0.93;
+      imgStyle.opacity       = 1.0;
+      imgStyle.width         = dim.thumb.w;
+      imgStyle.height        = dim.thumb.h;
+      titleStyle.height     -= 24;
+      imgStyle.padding       = '26px 27px 0px 27px';
+      style.width           += 20;
+      style.position         = 'relative';
+      descrStyle.visibility  = 'visible';
+      style.boxShadow        = '0px 0.1em 0.5em #000';
     }
 
     return (
+      <a href={'/boards/' + this.props.data.id} >
       <div
         onMouseOver={this.handleMouseOver}
         onMouseOut={this.handleMouseOut}
-        onClick={this.handleClick}
         style={style}
       >
         <center>
@@ -128,6 +127,7 @@ var BoardThumb = React.createClass({
           }
         </div>
       </div>
+      </a>
     );
   }
 });
@@ -151,9 +151,9 @@ var Main = React.createClass({
   render: function() {
     return (
       <div>
-      <TitleBar />
-      <BoardList data={testData} />
-    </div>
+        <TitleBar />
+        <BoardList data={testData} />
+      </div>
     );
   }
 });
