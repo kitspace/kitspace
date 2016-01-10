4c6a5978acbe1ce823ee4315fa3ea6ade73c0d2e
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-01-10 06:09:58 +0000

    Add titlebar in page

--- src/Page.jsx
+++ src/Page.jsx
@@ -6,13 +6,23 @@ const TitleBar      = require('./TitleBar');
 
 var Page = React.createClass({
   render: function () {
-    var title = info.id.split('/').slice(1).join(' / ');
+    var titleTxt = info.id.split('/').slice(1).join(' / ');
     return (
-      <DocumentTitle title={title}>
-        <div>
-          <TitleBar center/>
-          {title}
-        </div>
+      <DocumentTitle title={titleTxt}>
+        <TitleBar>
+          <div style={{
+              color: 'white'
+              , width: '100%'
+              , textAlign: 'center'
+              , verticalAlign: ' middle'
+              , lineHeight: '60px'
+              , fontSize: 24
+              , borderRadius: '5px'
+             }}
+          >
+            {titleTxt}
+          </div>
+        </TitleBar>
       </DocumentTitle>
     );
   },
