e85c8eefc16baff54c5509cc7a6cac8a39beef26
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-01-16 17:58:46 +0000

    Add a remote script to send to build server

---
+++ remote.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# used for rsyncing to a build server, set KITNIC_BUILD_SERVER to an rsync
+# accessible folder with this repo in it, use ./serve.sh on the server
+
+set -e
+
+if [ "$KITNIC_BUILD_SERVER" == "" ]; then
+    echo "You need to set KITNIC_BUILD_SERVER";
+    exit 1;
+fi
+
+function send() {
+    rsync -r . $KITNIC_BUILD_SERVER --update --delete --progress \
+        --exclude build --exclude node_modules --exclude .git;
+}
+
+send;
+
+while inotifywait --exclude '\..*sw.' -r -q -e modify ./ src tasks; do
+    send;
+done
--- serve.sh
+++ serve.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
+# this serves the build output and watches for changes to the source or tasks
+# to rebuild
+
+set -e
+
 rm -f /tmp/ninja.lock
+
 ./configure.coffee && ninja -v && http-server build/ &
+
 while inotifywait --exclude '\..*sw.' -r -q -e modify src tasks boards; do
     if [ ! -e "/tmp/ninja.lock" ]; then
         touch /tmp/ninja.lock; ninja && echo 'build succeeded'; rm /tmp/ninja.lock;
