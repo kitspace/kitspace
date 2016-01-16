26e86cc66eeca29dc8e66801df512a39d245b529
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-01-16 19:09:02 +0000

    Line wrap in Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -20,14 +20,29 @@ var BoardList = React.createClass({
     }
     var thumbNodes = this.props.data.map(function(data, index) {
       return (
-        <BoardThumb data={data} key={data.id + index} lazyLoad={ (! initialLoad) || (index > 10) }/>
+        <BoardThumb data={data} key={data.id + index}
+          lazyLoad={ (! initialLoad) || (index > 10) }/>
       );
     });
     var displayIntro = initialLoad ? 'inherit' : 'none';
     var intro = (
-      <div style={{marginLeft:'10%', marginRight:'10%', marginTop:32, marginBottom: 32, display:displayIntro}}>
+      <div style=
+        {{
+          marginLeft:'10%'
+          , marginRight:'10%'
+          , marginTop:32
+          , marginBottom: 32
+          , display:displayIntro
+      }}>
       <center>
-        <div style={{backgroundColor: '#FAFAFA', padding: 20, borderRadius: 5,  textAlign:'left', maxWidth:700}}>
+        <div style=
+          {{
+            backgroundColor: '#FAFAFA'
+            , padding: 20
+            , borderRadius: 5
+            , textAlign:'left'
+            , maxWidth:700
+        }}>
         <p>
 
           Kitnic.it is a repository of open hardware electronics projects that
@@ -37,9 +52,9 @@ var BoardList = React.createClass({
         </p><p>
 
           To quickly purchase the parts from various retailers you should
-          <a href='http://1clickBOM.com'> install</a> the 1-click-BOM extension. It's pretty useful
-          on it's own too and can be used on other sites. Read more about it
-          <a href='http://1clickBOM.com'> here</a>.
+          <a href='http://1clickBOM.com'> install</a> the 1-click-BOM extension.
+          It's pretty useful on it's own too and can be used on other sites.
+          Read more about it <a href='http://1clickBOM.com'>here</a>.
 
         </p><p>
 
@@ -90,9 +105,11 @@ var Main = React.createClass({
         _paq.push(['trackSiteSearch',
             // Search keyword searched for
             term,
-            // Search category selected in your search engine. If you do not need this, set to false
+            // Search category selected in your search engine. If you do not
+            // need this, set to false
             "Boards",
-            // Number of results on the Search results page. Zero indicates a 'No Result Search Keyword'. Set to false if you don't know
+            // Number of results on the Search results page. Zero indicates a
+            // 'No Result Search Keyword'. Set to false if you don't know
             result.length
         ]);
       }
