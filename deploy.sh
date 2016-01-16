f1690cab028c0c6ffaa2bbac65c7883d9ec4f58a
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-01-16 22:18:27 +0000

    Revert "Test out special preview deploy for pull-requests"
    This reverts commit bb72222920d86b0d3490e97c64c07b26bab74a9a.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#don't do anything on repo branches
-if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" != "master" ]
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ] || [ "${TRAVIS_BRANCH}" != "master" ]
     then exit 0;
 fi
 
@@ -14,13 +13,4 @@ git config user.name "Travis CI"
 git config user.email "travisCI@monostable.co.uk"
 git add .
 git commit -m "Deploy to GitHub Pages"
-
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-    git push --force --quiet \
-        "https://${GH_TOKEN}@github.com/monostable/preview.kitnic.it" \
-        master:gh-pages > /dev/null 2>&1
-else
-    git push --force --quiet \
-        "https://${GH_TOKEN}@github.com/monostable/kitnic" \
-        master:gh-pages > /dev/null 2>&1
-fi
+git push --force --quiet "https://${GH_TOKEN}@github.com/monostable/kitnic" master:gh-pages > /dev/null 2>&1
