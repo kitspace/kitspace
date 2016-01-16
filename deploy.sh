6eece89042f7e95752ae1d283231ddd949cd498b
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-01-16 22:18:25 +0000

    Revert "Amend CNAME setting in preview deploy"
    This reverts commit 6918334d1ca6f42f61e1bb27dfb22958dbb124b2.

--- deploy.sh
+++ deploy.sh
@@ -9,21 +9,17 @@ set -e
 cd build
 
 git init
+echo "kitnic.it" > CNAME
 git config user.name "Travis CI"
 git config user.email "travisCI@monostable.co.uk"
 git add .
+git commit -m "Deploy to GitHub Pages"
 
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-    echo "preview.kitnic.it" > CNAME
-    git add CNAME
-    git commit -m "Deploy to GitHub Pages"
     git push --force --quiet \
         "https://${GH_TOKEN}@github.com/monostable/preview.kitnic.it" \
         master:gh-pages > /dev/null 2>&1
 else
-    echo "kitnic.it" > CNAME
-    git add CNAME
-    git commit -m "Deploy to GitHub Pages"
     git push --force --quiet \
         "https://${GH_TOKEN}@github.com/monostable/kitnic" \
         master:gh-pages > /dev/null 2>&1
