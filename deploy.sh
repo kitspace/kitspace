bb72222920d86b0d3490e97c64c07b26bab74a9a
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-01-16 20:29:46 +0000

    Test out special preview deploy for pull-requests

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ] || [ "${TRAVIS_BRANCH}" != "master" ]
+#don't do anything on repo branches
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" != "master" ]
     then exit 0;
 fi
 
@@ -13,4 +14,13 @@ git config user.name "Travis CI"
 git config user.email "travisCI@monostable.co.uk"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/monostable/kitnic" master:gh-pages > /dev/null 2>&1
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+    git push --force --quiet \
+        "https://${GH_TOKEN}@github.com/monostable/preview.kitnic.it" \
+        master:gh-pages > /dev/null 2>&1
+else
+    git push --force --quiet \
+        "https://${GH_TOKEN}@github.com/monostable/kitnic" \
+        master:gh-pages > /dev/null 2>&1
+fi
