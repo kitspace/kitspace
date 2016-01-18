e7fb3997dc69cf3a7f293857ccb2ab398ccec14e
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-01-18 15:57:07 +0000

    Tweak deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# don't deploy on pull reqests or on branch builds
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ] || [ "${TRAVIS_BRANCH}" != "master" ]
     then exit 0;
 fi
@@ -12,5 +14,5 @@ echo "kitnic.it" > CNAME
 git config user.name "Travis CI"
 git config user.email "travisCI@monostable.co.uk"
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Deploy to GitHub Pages" > /dev/null
 git push --force --quiet "https://${GH_TOKEN}@github.com/monostable/kitnic" master:gh-pages > /dev/null 2>&1
