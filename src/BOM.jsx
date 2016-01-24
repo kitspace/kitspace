32613b359bd7790fd309ddbfa273ab75a13ceb63
Jake <jakepyne@gmail.com>
2016-01-24 14:32:50 +0100

    Dump out most of the BOM data

--- src/BOM.jsx
+++ src/BOM.jsx
@@ -1,10 +1,40 @@
 'use strict'
 const React = require('react');
+const _ = require('lodash');
 
 let BOM = React.createClass({
   render: function () {
+
+    // Pluck the object keys to use as headers
+    const keys = _.keys(_.head(this.props.items));
+
+    let headers = keys.map((item, index) => {
+      return ( <td key={index}>{ item }</td> );
+    });
+
+    let rows = this.props.items.map((item, index) => {
+
+      let row = keys.map((index) => {
+        // TODO retailer display
+        if (index !== 'retailers')
+          return ( <td key={index}>{ item[index] }</td> );
+
+        return ( <td key={index}>TODO: object</td> );
+      });
+
+      return ( <tr key={index}>{ row }</tr> );
+
+    });
+
     return (
-      <div></div>
+      <table>
+        <thead>
+          <tr>{ headers }</tr>
+        </thead>
+        <tbody>
+          { rows }
+        </tbody>
+      </table>
     )
   }
 });
