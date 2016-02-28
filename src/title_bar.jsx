bd2016bea6fec64438a6b81e84b1ba1aa89ab83e
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-02-28 19:52:46 +0000

    Mess about with media breakpoint

--- src/title_bar.jsx
+++ src/title_bar.jsx
@@ -2,17 +2,6 @@ const React       = require('react');
 const ReactDOM    = require('react-dom');
 
 const style = {
-  titleBar: {
-    backgroundColor:'#373737',
-    width:'100%',
-    boxShadow: '0px 0.1em 0.5em #000',
-    overflow: 'hidden',
-    borderBottomRightRadius: 5,
-    borderBottomLeftRadius: 5,
-    display:'flex',
-    flexWrap: 'wrap',
-  },
-
   submit: {
     height:'100%',
     lineHeight: '64px',
@@ -31,13 +20,15 @@ var TitleBar = React.createClass({
 
   render: function () {
     return (
-      <div style={style.titleBar}>
+      <div className='titleBar'>
         <div className='logoContainer'>
           <a href='/'>
-            <img style={{width:'90%'}} src='/images/logo.svg' />
+            <center>
+              <img style={{width:'70%'}} src='/images/logo.svg' />
+            </center>
           </a>
         </div>
-        <div className='title_bar middleContainer'>
+        <div className='middleContainer'>
           {this.props.children}
         </div>
         <a href='https://github.com/monostable/kitnic/#submitting-your-project-repo'>
