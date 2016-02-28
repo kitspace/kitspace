ddbb03ab4f66a0aaa66ea3bfe52395c329defc7b
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-02-28 00:05:06 +0000

    Change comma style in BoardThumb

--- src/BoardThumb.jsx
+++ src/BoardThumb.jsx
@@ -6,65 +6,67 @@ const FadeImage    = require('./FadeImage');
 
 const sizes = {
   thumb : {
-      w            : 240
-    , h            : 180
-    , captionH     : 48
-    , descriptionH : 96
+    w            : 240,
+    h            : 180,
+    captionH     : 48,
+    descriptionH : 96
   }
 }
 
 const style =  {
-    container: {
-      backgroundColor: '#373737'
-      , width: sizes.thumb.w + 32 + 16
-      , height: sizes.thumb.h + sizes.thumb.captionH
-          + sizes.thumb.descriptionH + 16 + 5 + 16
-      , borderRadius: 10
-      , marginTop:  8
-      , marginRight: 8
-      , marginBottom: 8
-      , marginLeft: 8
-      , display: 'inline-block'
-      , border: '1px solid #CFCFCF'
-      , cursor: 'pointer'
-      , overflow: 'hidden'
-    },
-    img: {
-      width: sizes.thumb.w
-      , height: sizes.thumb.h
-      , marginTop: 16
-      , marginBottom: 16
-    },
-    title: {
-      height: sizes.thumb.captionH
-      , lineHeight: String(sizes.thumb.captionH) + 'px'
-      , textAlign: 'center'
-      , verticalAlign: 'middle'
-      , fontWeight: 'bold'
-      , fontSize: 18
-      , paddingBottom: 5
-      , opacity: 1.0
-      , backgroundColor: '#FAFAFA'
-      , color: '#373737'
-    },
-    description: {
-      visibility: 'visible'
-      , height: sizes.thumb.descriptionH
-      , overflow: 'hidden'
-      , padding: '10 10 10 10'
-      , textAlign: 'center'
-      , verticalAlign: 'top'
-      , fontSize: 16
-      , marginBottom: 10
-      , opacity: 1.0
-      , backgroundColor: '#FAFAFA'
-      , color : '#373737'
-      , fontWeight: 'normal'
-    }
+  container: {
+    backgroundColor : '#373737',
+    width           : sizes.thumb.w + 32 + 16,
+    height          :
+      sizes.thumb.h + sizes.thumb.captionH
+        + sizes.thumb.descriptionH + 16 + 5 + 16,
+    borderRadius    : 10,
+    marginTop       : 8,
+    marginRight     : 8,
+    marginBottom    : 8,
+    marginLeft      : 8,
+    display         : 'inline-block',
+    border          : '1px solid #CFCFCF',
+    cursor          : 'pointer',
+    overflow        : 'hidden'
+  },
+  img: {
+    width        : sizes.thumb.w,
+    height       : sizes.thumb.h,
+    marginTop    : 16,
+    marginBottom : 16
+  },
+  title: {
+    height          : sizes.thumb.captionH,
+    lineHeight      : String(sizes.thumb.captionH) + 'px',
+    textAlign       : 'center',
+    verticalAlign   : 'middle',
+    fontWeight      : 'bold',
+    fontSize        : 18,
+    paddingBottom   : 5,
+    opacity         : 1.0,
+    backgroundColor : '#FAFAFA',
+    color           : '#373737'
+  },
+  description: {
+    visibility      : 'visible',
+    height          : sizes.thumb.descriptionH,
+    overflow        : 'hidden',
+    padding         : '10 10 10 10',
+    textAlign       : 'center',
+    verticalAlign   : 'top',
+    fontSize        : 16,
+    marginBottom    : 10,
+    opacity         : 1.0,
+    backgroundColor : '#FAFAFA',
+    color           : '#373737',
+    fontWeight      : 'normal'
+  }
 }
 
 var BoardThumb = React.createClass({
   render: function () {
+
     if (this.props.lazyLoad) {
       var image =
           <LazyLoad once={true}
