43fd5ef63ecfdf52cc5dcc8999a048611797d39a
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-03-05 22:56:10 +0000

    Fix duplicate require

--- src/page.jsx
+++ src/page.jsx
@@ -1,11 +1,9 @@
 const React         = require('react');
 const ReactDOM      = require('react-dom');
 const DocumentTitle = require('react-document-title');
-const TitleBar      = require('./TitleBar');
-const FadeImage     = require('./FadeImage');
-const BOM           = require('./BOM');
 const TitleBar      = require('./title_bar');
 const FadeImage     = require('./fade_image');
+const BOM           = require('./BOM');
 
 const info    = require('./info.json');
 const zipPath = require('./zip-info.json');
