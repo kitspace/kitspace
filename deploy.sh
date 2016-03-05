4d69558ca3ebd3b9afcd37bec3f5feed7d15d383
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-03-05 16:03:34 +0000

    Add a branch preview deploy

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# don't deploy on pull reqests or on branch builds
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ] || [ "${TRAVIS_BRANCH}" != "master" ]
-    then exit 0;
-fi
-
+#exit with non-zero exit code if any process fails
 set -e
 
-cd build
-
-git init
-echo "kitnic.it" > CNAME
-git config user.name "Travis CI"
-git config user.email "travisCI@monostable.co.uk"
-git add .
-git commit -m "Deploy to GitHub Pages" > /dev/null
-git push --force --quiet "https://${GH_TOKEN}@github.com/monostable/kitnic" master:gh-pages > /dev/null 2>&1
+#  don' do anything on pull-requests
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]
+then
+    exit 0
+elif  [ "${TRAVIS_BRANCH}" != "master" ]
+then
+    echo -e "${SSH_KEY}" > key
+    chmod 600 key
+    scp -i key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r build "ubuntu@preview.kitnic.it:www/kitnic/${TRAVIS_BRANCH}"
+    rm key
+else
+    cd build
+    git init
+    echo "kitnic.it" > CNAME
+    git config user.name "Travis CI"
+    git config user.email "travisCI@monostable.co.uk"
+    git add .
+    git commit -m "Deploy to GitHub Pages" > /dev/null
+    git push --force --quiet "https://${GH_TOKEN}@github.com/monostable/kitnic" master:gh-pages > /dev/null 2>&1
+fi
