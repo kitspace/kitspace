347db8cc5c0eca71d1a20c77aa4ed5c5c374511d
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-03-06 00:04:19 +0000

    Make it spit out retailer data

--- src/bom.jsx
+++ src/bom.jsx
@@ -6,21 +6,25 @@ let BOM = React.createClass({
   render: function () {
 
     // Pluck the object keys to use as headers
-    const keys = _.keys(_.head(this.props.items));
+    const keys = _.keys(this.props.items[0]);
+    const retailers = _.keys(this.props.items[1].retailers)
 
-    let headers = keys.map((item, index) => {
-      return ( <td key={index}>{ item }</td> );
+    let headers = keys.concat(retailers).map((item, index) => {
+      if (item !== 'retailers' && item !== 'row') {
+        return ( <td key={`header-${index}`}>{ item }</td> );
+      }
     });
 
     let rows = this.props.items.map((item, index) => {
 
-      let row = keys.map((index) => {
-        // TODO retailer display
-        if (index !== 'retailers')
-          return ( <td key={index}>{ item[index] }</td> );
-
-        return ( <td key={index}>TODO: object</td> );
+      let row = keys.map((key) => {
+        if (key !== 'retailers' && key !== 'row') {
+            return ( <td key={key}>{ item[key] }</td> );
+        }
       });
+      row = row.concat(retailers.map((key) => {
+        return ( <td key={`${item}-${key}`}>{ item.retailers[key] }</td> );
+      }));
 
       return ( <tr key={index}>{ row }</tr> );
 
