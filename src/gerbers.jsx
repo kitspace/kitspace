2f0acbba3305c885248ef54d8ddaa0666354c905
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-04-27 19:57:12 +0100

    Fix missing gerbers link

--- src/gerbers.jsx
+++ src/gerbers.jsx
@@ -3,18 +3,18 @@ const React = require('react');
 const zipPath = require('./zip-info.json');
 
 let Gerbers = React.createClass({
-	render: function() {
-		return (
-			<div className="gerbersContainer">
-				<a className="zipPath" ref={zipPath}>
-					<div className="gerbers">
-						<i className="octicon octicon-circuit-board"></i>
-						{` `}Download Gerbers
-					</div>
-				</a>
-			</div>
-		);
-	}
+  render: function() {
+    return (
+      <div className="gerbersContainer">
+        <a className="zipPath" href={zipPath}>
+          <div className="gerbers">
+            <i className="octicon octicon-circuit-board"></i>
+            {' '}Download Gerbers
+          </div>
+        </a>
+      </div>
+    );
+  }
 });
 
 module.exports = Gerbers;
