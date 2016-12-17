ffc600024abb3895269999cafb098d410e1125ba
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-17 23:22:07 +0000

    Start instructions

--- src/submit.jsx
+++ src/submit.jsx
@@ -1,11 +1,16 @@
-const React       = require('react')
-const TitleBar    = require('./title_bar')
+const Markdown = require('react-markdown')
+
+const React         = require('react')
+const TitleBar      = require('./title_bar')
 
 var Submit = React.createClass({
   getInitialState: function() {
     return null
   },
   render: function () {
+    var input =`1. Export your plotted gerbers & drill data and put them into a \`gerbers/\` directory.
+    If you would like to put them somewhere else please add a kitnic.yaml with a field \`gerbers:\` followed by the path to the directory.`
+
     return (
       <div>
         <TitleBar>
@@ -13,6 +18,7 @@ var Submit = React.createClass({
               Register a project
             </div>
         </TitleBar>
+        <Markdown className='instructions' source={input} />
       </div>
     )
   },
