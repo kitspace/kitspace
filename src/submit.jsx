67c6e2c39481ea8dae668c6600c1bcf414e8852f
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-18 18:46:15 +0000

    Add steps to submit instructions

--- src/submit.jsx
+++ src/submit.jsx
@@ -1,41 +1,53 @@
 const Markdown = require('react-markdown')
 
 const React     = require('react')
-const { Input } = require('semantic-ui-react')
+const { Input, Icon, Step } = require('semantic-ui-react')
 
 const TitleBar  = require('./title_bar')
+
+const instructionText =
+`
+- Plot Gerbers (RS274-X) & drill data from your CAD program.
+- Put the files into a \`gerbers/\` directory in a publicly accessible git repository
+(you could use [GitLab](https://gitlab.com) or [GitHub](https://github.com) for instance).
+
+If you would like to put them somewhere else in your repository please also add a
+kitnic.yaml with a field \`gerbers:\` followed by the path to the directory so Kitnic can find it.
+Use forward slashes as path seperators, e.g. \`gerbers: hardware/gerbers\`.
+
+Preview your board by entering the repository URL below.
+`
+
+const Steps = () => (
+  <div>
+    <Step.Group ordered>
+      <Step active={true}>Preview the board</Step>
+      <Step>Preview the bill of materials</Step>
+      <Step>Preview the readme</Step>
+    </Step.Group>
+  </div>
+)
 
 var Submit = React.createClass({
   getInitialState: function() {
     return null
   },
   render: function () {
-    var input =
-`# Step 1
-
-- Plot Gerbers & drill data from your CAD program.
-- Put the files into a \`gerbers/\` directory.
-- Add this directory to a publicly accessible git repository (like on [GitLab](https://gitlab.com) or [GitHub](https://github.com)).
-
-If you would like to put them somewhere else in your repository please also add a
-kitnic.yaml with a field \`gerbers:\` followed by the path to the directory
-(using forward slashes as path seperators, e.g. \`gerbers: hardware/gerbers\`).
-
-Preview your board by entering the repository URL below.
-`
-
     return (
-      <div>
-        <TitleBar>
-            <div className='titleText'>
-              Submit a project
-            </div>
-        </TitleBar>
-        <Markdown className='instructions' source={input} />
+    <div>
+      <TitleBar>
+        <div className='titleText'>
+          Submit a project
+        </div>
+      </TitleBar>
+      <div className='content'>
+        <Steps />
+        <Markdown className='instructions' source={instructionText} />
         <div className='previewContainer'>
-            <Input fluid action={{color:'green', content:'preview'}} placeholder='https://github.com/kitnic-forks/arduino-uno' />
+          <Input fluid action={{color:'green', content:'preview'}} placeholder='https://github.com/kitnic-forks/arduino-uno' />
         </div>
       </div>
+    </div>
     )
   },
   componentDidMount: function() {
