76b3c1e9b69a5e41719c7fcf5d24b5c35a5f23fa
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-01-18 22:10:06 +0000

    Fix media query for tablets

--- src/media_queries.js
+++ src/media_queries.js
@@ -1,15 +1,11 @@
 //these are also defined in common.scss and need to be adjusted there and here
-const mobile = `only screen
-  and (min-device-width: 320px)
-  and (max-device-width: 480px)
-  , (orientation: landscape) and (max-device-width: 810px)
-  , (handheld)
+const mobile = `
+  (orientation: portrait) and (max-device-width: 480px)
+  , (orientation: landscape) and (max-device-width: 660px)
 `
-const mobile_or_small_width = `only screen
-  and (min-device-width: 320px)
-  and (max-device-width: 480px)
-  , (orientation: landscape) and (max-device-width: 810px)
-  , (handheld)
+const mobile_or_small_width = `
+  (orientation: portrait) and (max-device-width: 480px)
+  , (orientation: landscape) and (max-device-width: 660px)
   , (max-width: 810px)
 `
 const small_width = '(max-width: 810px)'
