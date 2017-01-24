ce74997a51de729f0ea66033156332caa4d35204
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-01-24 18:52:28 +0000

    Shorten gerber step instruction text

--- src/submit/submit.jsx
+++ src/submit/submit.jsx
@@ -81,14 +81,9 @@ const store = Redux.createStore(reducer)
 
 const instructionTexts = [
 `
-Plot Gerbers (RS274-X) & drill data from your CAD program. Put the files into a
-\`gerbers/\` directory in a publicly accessible git repository (you could use
-[GitLab](https://gitlab.com) or [GitHub](https://github.com) for instance).
-
-If you would like to put them somewhere else in your repository please also add
-a kitnic.yaml with a field \`gerbers:\` followed by the path to the directory
-so Kitnic can find it.  Use forward slashes as path seperators, e.g. \`gerbers:
-hardware/gerbers\`.
+Plot Gerbers (RS274-X) & drill data from your CAD program. Put the files in a
+publicly accessible Git repository (you could use [GitHub](https://github.com)
+or [GitLab](https://gitlab.com) for instance).
 
 Preview your board by entering the repository URL below.
 `,
