6ee6bb7eb918b3f29d215a0070422fc1cf8f18fa
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-02-01 17:02:53 +0000

    Add instructions and next button for step3

--- src/submit/step3.jsx
+++ src/submit/step3.jsx
@@ -1,11 +1,9 @@
 const React    = require('react')
 const Markdown = require('react-markdown')
 const marky    = require('marky-markdown')
-const {
-  Container,
-} = require('semantic-ui-react')
+const semantic = require('semantic-ui-react')
 
-const Steps = require('./steps')
+const Steps     = require('./steps')
 const UrlSubmit = require('./url_submit')
 
 const TitleBar = require('../title_bar')
@@ -13,6 +11,20 @@ const TitleBar = require('../title_bar')
 const Step3 = React.createClass({
   render() {
     const board = this.props.board
+    let nextButton, instructionText
+    if (board.readme) {
+      nextButton = <semantic.Button
+        content       = 'Next'
+        icon          = 'right arrow'
+        labelPosition = 'right'
+        color         = 'green'
+        onClick       = {this.props.setStep(4)} />
+    }
+    else if (board.status === 'done' || board.status === 'not sent'){
+      instructionText = 'Add a README.md to your repository explaining more about'
+      + ' the project. This should be writtern in [GitHub Flavored Markdown]'
+      + '(https://help.github.com/articles/basic-writing-and-formatting-syntax/).'
+    }
     return (
     <div className='Step Step3'>
       <TitleBar>
@@ -20,16 +32,17 @@ const Step3 = React.createClass({
           {'Submit a project'}
         </div>
       </TitleBar>
-      <Container>
+      <semantic.Container>
         <Steps setStep={this.props.setStep} active={3}/>
-        <Markdown className='instructions' source={''} />
         <div className='userInputSegment'>
           <UrlSubmit dispatch={this.props.dispatch} board={board} />
+          <div className='nextButtonContainer'>
+            {nextButton}
+          </div>
         </div>
-        <div>
-          {board.readme}
-        </div>
-      </Container>
+        <Markdown className='instructions' source={instructionText} />
+        {board.readme}
+      </semantic.Container>
     </div>
     )
   },
