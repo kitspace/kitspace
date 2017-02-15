7fb3dbe00b899613e6536f3585fe7f6d4ff0dfb4
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-02-15 19:33:50 +0100

    Improve datasheet and image data shape

--- src/octopart.js
+++ src/octopart.js
@@ -1,22 +1,39 @@
 const superagent = require('superagent')
 const apikey = require('./secrets').OCTOPART_API_KEY
 
-const queries = [{mpn:'ATMEGA32U4AUR'}, {mpn: 'NE555P'}]
 
 function description(items) {
   return items.reduce((prev, x) => prev || x.short_description, null)
 }
 
-function imageset(items) {
+function datasheet(items) {
   return items.reduce((prev, x) => {
-    return prev || x.imagesets.reduce((prev, set) => prev || set, null)
+    return x.datasheets.reduce((prev, d) => prev || d.url, prev)
+  }, null)
+}
+
+function image(items) {
+  return items.reduce((prev, x) => {
+    return prev || x.imagesets.reduce((prev, set) => {
+      if (prev != null) {
+        return prev
+      }
+      if (set.medium_image && set.medium_image.url) {
+        return {
+          url: set.medium_image.url,
+          credit_string: set.credit_string,
+          credit_url: set.credit_url
+        }
+      }
+      return null
+    }, null)
   }, null)
 }
 
 function octopart(queries) {
   const indexed = queries.map((q, index) => Object.assign(q, {reference: index}))
   return superagent.get('https://octopart.com/api/v3/parts/match')
-    .query('include[]=short_description&include[]=imagesets')
+    .query('include[]=short_description&include[]=imagesets&include[]=datasheets')
     .query({
       apikey,
       queries: JSON.stringify(indexed),
@@ -32,10 +49,12 @@ function octopart(queries) {
         const items = result.items
         return Object.assign(q, {
           description: description(items),
-          imageset: imageset(items)
+          image: image(items),
+          datasheet: datasheet(items),
         })
       })
     })
 }
 
+const queries = [{mpn:'ATMEGA32U4AUR'}, {mpn: 'NE555P'}]
 octopart(queries).then(r => console.log(r))
