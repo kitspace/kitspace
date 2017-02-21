073d574aee2261c9fc2695ed51421248ae4d1414
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-02-21 14:17:06 +0100

    Work on query life cycle

--- src/reducers.js
+++ src/reducers.js
@@ -2,13 +2,19 @@ const redux     = require('redux')
 const immutable = require('immutable')
 
 const initial_state = immutable.Map({
-  queries: immutable.List()
+  queries: immutable.List(),
+  query_time: Date.now(),
+  responses: immutable.List(),
 })
 
 
 const reducers = {
   addQuery(state, query) {
-    return state.set('queries', state.get('queries').push(query))
+    const queries = state.get('queries')
+    if (queries.size === 0) {
+      state = state.set('query_time', Date.now())
+    }
+    return state.set('queries', queries.push(query))
   },
   clearQueries(state) {
     return state.set('queries', immutable.List())
