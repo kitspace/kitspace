6371eea5b2d4925f74ac255e21d872f3e76b32cf
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-02-21 21:54:39 +0100

    Start integration testing

--- integration/test_api.js
+++ integration/test_api.js
@@ -1,5 +1,6 @@
 const graphqlTester = require('graphql-tester')
 const createExpressWrapper = require('graphql-tester/lib/main/servers/express.js').create
+const assert = require('assert')
 
 const app = require('../src/app')
 
@@ -9,7 +10,15 @@ describe('API', () => {
     url: '/graphql'
   })
   it('responds', done => {
-    test('{ fromMpn(mpn: "NE555P") }').then(response => {
+    test('{fromMpn(mpn: {mpn: "NE555P"}) {description} }').then(response => {
+      assert(response.success)
+      assert(response.status === 200)
+      assert(response.data.fromMpn != null)
+      return done()
+    })
+  })
+  it('responds twice', done => {
+    test('{fromMpn(mpn: {mpn: "NE555P"}) {description} }').then(response => {
       assert(response.success)
       assert(response.status === 200)
       assert(response.data.fromMpn != null)
