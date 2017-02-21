53d7f6fa94cdf7ae7f79ab57257e8dacdf350fe3
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-02-21 15:10:26 +0100

    Work on reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,6 +1,9 @@
 const redux     = require('redux')
 const immutable = require('immutable')
 
+//24hrs
+const TIMEOUT_MS = 24 * 60 * 60 * 1000 //milliseconds
+
 const initial_state = immutable.Map({
   queries: immutable.List(),
   responses: immutable.List(),
@@ -12,8 +15,22 @@ const reducers = {
     const queries = state.get('queries')
     return state.set('queries', queries.push(query))
   },
-  clearQueries(state) {
-    return state.set('queries', immutable.List())
+  clearQueries(state, queriesToClear) {
+    const queries = state.get('queries').filter(q => {
+      return !queriesToClear.contains(q)
+    })
+    return state.set('queries', queries)
+  },
+  addResponse(state, response) {
+    const responses = state.get('responses')
+    return state.set('responses', responses.push(response))
+  },
+  clearTimedout(state) {
+    const now = Date.now()
+    const responses = state.get('responses').filter(r => {
+      return (now - r.get('time')) < TIMEOUT_MS
+    })
+    return state.set('responses', responses)
   },
 }
 
