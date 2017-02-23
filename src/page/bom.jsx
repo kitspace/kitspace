689f6f8ed6e565031f7790d716b56b5a68dcdd2c
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-02-23 16:58:25 +0100

    Make cells selectable and try out popup

--- src/page/bom.jsx
+++ src/page/bom.jsx
@@ -2,7 +2,7 @@
 const React           = require('react')
 const DoubleScrollbar = require('react-double-scrollbar')
 const {h, table, thead, tbody, tr, th, td} = require('react-hyperscript-helpers')
-const {Table} = require('semantic-ui-react')
+const semantic = require('semantic-ui-react')
 
 //for react-double-scrollbar in IE11
 require('babel-polyfill')
@@ -33,10 +33,10 @@ function tsvToTable(tsv) {
   const lines = tsv.split('\n').slice(0, -1)
   const headings = lines[0].split('\t')
   let headingJSX = headings.map((text) => {
-    return h(Table.HeaderCell, {selectable: true}, text)
+    return h(semantic.Table.HeaderCell, {selectable: true}, text)
   })
-  headingJSX = h(Table.Header, [h(Table.Row, headingJSX)])
-  const markPink = (index) => {
+  headingJSX = h(semantic.Table.Header, [h(semantic.Table.Row, headingJSX)])
+  function markPink(index) {
     return ['Manufacturer', 'MPN', 'Description']
       .indexOf(headings[index]) < 0
   }
@@ -45,11 +45,17 @@ function tsvToTable(tsv) {
     return tr(`.tr${rowIndex % 2}`, line.map((text, columnIndex) => {
       const error = markPink(columnIndex) && text == ''
       const className = columnIndex === 0 ? 'marked ' + markerColor(text) : ''
-      return h(Table.Cell, {error, className}, text)
+      const cell = h(semantic.Table.Cell, {error, className, selectable:true}, text)
+      if (headings[columnIndex] === 'MPN') {
+        return (<semantic.Popup trigger={cell} position='bottom left' content='reaaaaaaaaaaaaaaaally long text and stuff, you could have an image here' />)
+      }
+      else {
+        return cell
+      }
     }))
   }))
   const tableProps = {celled: true, unstackable: true, selectable: true}
-  return h(Table, tableProps, [headingJSX, bodyJSX])
+  return h(semantic.Table, tableProps, [headingJSX, bodyJSX])
 }
 
 let BOM = React.createClass({
