0ec7eb3a1752df501a866935d825ec09c91b6792
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-02-24 12:44:10 +0100

    Refactor page.jsx

--- src/page/page.jsx
+++ src/page/page.jsx
@@ -1,45 +1,47 @@
 const React         = require('react')
 const DocumentTitle = require('react-document-title')
-const TitleBar      = require('../title_bar')
+
 const BOM           = require('./bom')
 const BoardShowcase = require('./board_showcase')
 const StoreButtons  = require('./buy_parts')
-const info          = require('../info.json')
-const Readme        = require('../readme')
 const Gerbers       = require('./gerbers')
-const FadeImage     = require('../fade_image')
 const InfoBar       = require('./info_bar')
 
-var Page = React.createClass({
-  render: function () {
-    const titleTxt = info.id.split('/').slice(2).join(' / ')
-    const subtitleTxt = info.id.split('/').slice(0,2).join(' / ')
+const TitleBar  = require('../title_bar')
+const FadeImage = require('../fade_image')
+const info      = require('../info.json')
+const Readme    = require('../readme')
+
+const Page = React.createClass({
+  render() {
+    const titleText    = info.id.split('/').slice(2).join(' / ')
+    const subtitleText = info.id.split('/').slice(0,2).join(' / ')
     return (
-      <DocumentTitle title={`${titleTxt} - kitnic.it`}>
-      <div>
-        <div className='page'>
-          <TitleBar submissionButton={true}>
-            <div className='titleText'>
-              {titleTxt}
+      <DocumentTitle title={`${titleText} - kitnic.it`}>
+        <div>
+          <div className='page'>
+            <TitleBar submissionButton={true}>
+              <div className='titleText'>
+                {titleText}
+              </div>
+              <div className='subtitleText'>
+                {subtitleText}
+              </div>
+            </TitleBar>
+            <div className="pageContainer">
+              <InfoBar info={info} />
+              <Gerbers />
+              <BoardShowcase>
+                <FadeImage src='images/top.svg' />
+                <FadeImage src='images/bottom.svg'/>
+              </BoardShowcase>
+              <StoreButtons items={info.bom.lines} />
+              <Readme />
+              <BOM tsv={info.bom.tsv} />
             </div>
-            <div className='subtitleText'>
-              {subtitleTxt}
-            </div>
-          </TitleBar>
-          <div className="pageContainer">
-            <InfoBar info={info} />
-            <Gerbers />
-            <BoardShowcase>
-              <FadeImage src='images/top.svg' />
-              <FadeImage src='images/bottom.svg'/>
-            </BoardShowcase>
-            <StoreButtons items={ info.bom.lines ? info.bom.lines : [] } />
-            <Readme />
-            <BOM tsv={ info.bom ? info.bom.tsv : '' } />
           </div>
         </div>
-      </div>
-    </DocumentTitle>
+      </DocumentTitle>
     )
   },
 })
