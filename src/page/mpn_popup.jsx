f8b3aceecc4be69bf1ca4a8a1e5439ec43b43a4e
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-03-01 15:53:28 +0000

    Work on collapsed specs

--- src/page/mpn_popup.jsx
+++ src/page/mpn_popup.jsx
@@ -1,21 +1,35 @@
 const React       = require('react')
 const {h, a, div} = require('react-hyperscript-helpers')
 const semantic    = require('semantic-ui-react')
+const ramda       = require('ramda')
 
 const FadeImage = require('../fade_image')
 
 
-const important = ['capacitance', 'resistance', 'case_package', 'resistance_tolerance', 'capacitance_tolerance']
+const importance = [
+  ['capacitance', 'resistance'],
+  ['case_package'],
+  ['dielectric_characteristic'],
+  ['resistance_tolerance', 'capacitance_tolerance'],
+  ['voltage_rating', 'power_rating'],
+  ['case_package_si'],
+]
 
 function reorder(specs) {
-  return specs.reduce((acc, spec) => {
-    if (important.indexOf(spec.key) >= 0) {
-      acc.unshift(spec)
-    } else {
-      acc.push(spec)
+  const groups = specs.reduce((acc, spec) => {
+    let index = importance.reduce((prev, keys, index) => {
+      if (keys.indexOf(spec.key) >= 0) {
+        return index
+      }
+      return prev
+    }, null)
+    if (index == null) {
+      index = acc.length - 1
     }
+    acc[index].push(spec)
     return acc
-  }, [])
+  }, importance.map(x => []).concat([[]]))
+  return ramda.flatten(groups)
 }
 
 function MpnPopup(props) {
@@ -39,15 +53,15 @@ function MpnPopup(props) {
     ]),
     a({href: props.datasheet}, 'datasheet'),
     h(semantic.Divider),
-    div(props.description),
-    h(semantic.Divider),
-    h(semantic.Table, {very: true, basic: true, collapsing: true, celled: true}
-    , specs.map(spec => {
+    div(part.description),
+    h(semantic.Table, {basic: 'very', collapsing: true, celled: true}
+    , specs.slice(0,4).map(spec => {
       return h(semantic.Table.Row, [
         h(semantic.Table.Cell, spec.name),
         h(semantic.Table.Cell, spec.value),
       ])
-    }))
+    })),
+    h(semantic.Button, {fluid: true},  '...')
   ])
 }
 
