75d4371853c3c2b862f5466da0c9ba3addfe9436
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-03-01 18:06:19 +0000

    Make mpn popup expanding work

--- src/page/mpn_popup.jsx
+++ src/page/mpn_popup.jsx
@@ -32,51 +32,76 @@ function reorder(specs) {
   return ramda.flatten(groups)
 }
 
-function MpnPopup(props) {
-  const custom = {
-    className       : 'MpnPopup',
-    hoverable       : true,
-    mouseLeaveDelay : 200,
-    mouseEnterDelay : 200,
-    position        : 'bottom left',
-    trigger         : props.trigger,
-    onOpen          : props.onOpen,
-    onClose         : props.onClose,
-    wide            : true,
-  }
-  const part = props.part
-  const image = part.image || {}
-  const specs = reorder(part.specs || [])
-  return h(semantic.Popup, custom, [
-    div({className: 'topAreaContainer'}, [
-      div({style: {fontSize: 10}}, [
-        div({className: 'imageContainer'}, [
-          h(semantic.Image, {src: image.url}),
-        ]),
-        a({href: image.credit_url}, image.credit_string),
-      ]),
-      div({className: 'linkContainer datasheetLinkContainer'}, [
-        div([a({href: part.datasheet}, [
-          h(semantic.Icon, {name: 'file pdf outline'}),
-          'Datasheet'
-        ])])
-      ]),
-    ]),
-    h(semantic.Divider),
-    div(part.description),
-    h(semantic.Table, {basic: 'very', collapsing: true, celled: true}
-    , specs.slice(0,4).map(spec => {
-      return h(semantic.Table.Row, [
-        h(semantic.Table.Cell, spec.name),
-        h(semantic.Table.Cell, spec.value),
-      ])
-    })),
-    h(semantic.Button, {fluid: true},  '...'),
-    h(semantic.Divider),
-    div({className: 'linkContainer octopartLinkContainer'}, [
-      a({href: 'https://octopart.com/'}, 'Powered by Octopart')
-    ]),
+function specRow(spec) {
+  return h(semantic.Table.Row, [
+    h(semantic.Table.Cell, spec.name),
+    h(semantic.Table.Cell, spec.value),
   ])
 }
+
+const MpnPopup = React.createClass({
+  getInitialState() {
+    return {expanded: false}
+  },
+  toggleExpand() {
+    this.setState({expanded: !this.state.expanded})
+  },
+  render() {
+    const props = this.props
+    const custom = {
+      className       : 'MpnPopup',
+      hoverable       : true,
+      mouseLeaveDelay : 200,
+      mouseEnterDelay : 200,
+      position        : 'bottom left',
+      trigger         : props.trigger,
+      onOpen          : props.onOpen,
+      onClose         : props.onClose,
+      wide            : true,
+    }
+    const part = props.part
+    const image = part.image || {}
+    const specs = reorder(part.specs || [])
+    let tableRows
+    if (this.state.expanded) {
+      tableRows = specs.map(specRow)
+    }
+    else {
+      tableRows = specs.slice(0,4).map(specRow)
+    }
+    return h(semantic.Popup, custom, [
+      div({className: 'topAreaContainer'}, [
+        div({style: {fontSize: 10}}, [
+          div({className: 'imageContainer'}, [
+            h(semantic.Image, {src: image.url}),
+          ]),
+          a({href: image.credit_url}, image.credit_string),
+        ]),
+        div({className: 'linkContainer'}, [
+          div([a({href: part.datasheet}, [
+            h(semantic.Icon, {name: 'file pdf outline'}),
+            'Datasheet'
+          ])])
+        ]),
+      ]),
+      h(semantic.Divider),
+      div(part.description),
+      h(semantic.Table, {
+        basic: 'very',
+        collapsing: true,
+        celled: true
+      }, tableRows),
+      h(semantic.Button, {
+        onClick: this.toggleExpand,
+        fluid: true,
+        basic: true,
+      },  this.state.expanded ? '⇡' : '...'),
+      h(semantic.Divider),
+      div({className: 'linkContainer octopartLinkContainer'}, [
+        a({href: 'https://octopart.com/'}, 'Powered by Octopart')
+      ]),
+    ])
+  },
+})
 
 module.exports = MpnPopup
