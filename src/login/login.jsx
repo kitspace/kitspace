665d3e2a6a53f5502372ec21b2f9347101794b19
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-04-08 22:03:55 -0300

    Add some styling to twitter login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -75,8 +75,9 @@ const Login = React.createClass({
               <semantic.Form.Input type='submit'   name='commit' value='Log in' />
             </semantic.Form>
             <semantic.Form method='post' action='/gitlab/users/auth/twitter'>
+              <semantic.Label>{'Login with:'}</semantic.Label>
               <semantic.Form.Input type='hidden'   name='authenticity_token' value={this.state.token} />
-              <semantic.Form.Input type='submit'   name='commit' value='Twitter' />
+              <semantic.Form.Button type='submit'   name='commit' value='Twitter' icon='twitter' />
             </semantic.Form>
           </semantic.Sidebar.Pusher>
         </semantic.Sidebar.Pushable>
