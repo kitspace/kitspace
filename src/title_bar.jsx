59ed0f6c249acbdaa5be246e134a5d7c90b6b74a
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-04-10 20:06:34 -0300

    Add logged in avatar to title bar

--- src/title_bar.jsx
+++ src/title_bar.jsx
@@ -1,13 +1,39 @@
-const React = require('react')
-const semantic = require('semantic-ui-react')
+const React      = require('react')
+const semantic   = require('semantic-ui-react')
+const superagent = require('superagent')
 
-var TitleBar = React.createClass({
+const TitleBar = React.createClass({
   propTypes: {
     children: React.PropTypes.any,
     submissionButton: React.PropTypes.bool,
   },
-  render: function () {
-    const menu = null
+  getInitialState() {
+    return {
+      user: null
+    }
+  },
+  componentWillMount() {
+    superagent.get('/gitlab/api/v4/user')
+      .set('Accept', 'application/json')
+      .withCredentials()
+      .then(r => this.setState({user: r.body}))
+  },
+  render() {
+    const user = this.state.user
+    let button = (
+      <a href='/sign_in'>
+        <semantic.Button basic inverted>
+          {'Sign in'}
+        </semantic.Button>
+      </a>
+    )
+    if (user != null) {
+      button = (
+        <a>
+          <semantic.Image size='mini' shape='rounded' src={user.avatar_url} />
+        </a>
+      )
+    }
     return (
       <div className='titleBar'>
         <div className='logoContainer'>
@@ -19,7 +45,7 @@ var TitleBar = React.createClass({
           {this.props.children}
         </div>
         <div className='submitContainer'>
-          {menu}
+          {button}
         </div>
       </div>
     )
