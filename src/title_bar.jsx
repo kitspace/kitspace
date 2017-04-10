b25ab30ed677dde2d475504a60c91a8945b84492
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-04-10 20:29:53 -0300

    Add initial logout menu item

--- src/title_bar.jsx
+++ src/title_bar.jsx
@@ -2,6 +2,21 @@ const React      = require('react')
 const semantic   = require('semantic-ui-react')
 const superagent = require('superagent')
 
+function getLogoutToken() {
+  return superagent.get('/gitlab/profile')
+    .withCredentials()
+    .then(r => {
+      return (new DOMParser).parseFromString(r.text, 'text/html')
+    }).then(doc => {
+      const input = doc.querySelector('input[name=authenticity_token]')
+      if (input == null) {
+        return ''
+      }
+      return input.value
+    })
+}
+
+
 const TitleBar = React.createClass({
   propTypes: {
     children: React.PropTypes.any,
@@ -9,7 +24,8 @@ const TitleBar = React.createClass({
   },
   getInitialState() {
     return {
-      user: null
+      user: null,
+      logoutToken: '',
     }
   },
   componentWillMount() {
@@ -17,6 +33,8 @@ const TitleBar = React.createClass({
       .set('Accept', 'application/json')
       .withCredentials()
       .then(r => this.setState({user: r.body}))
+
+    getLogoutToken().then(logoutToken => this.setState({logoutToken}))
   },
   render() {
     const user = this.state.user
@@ -29,9 +47,24 @@ const TitleBar = React.createClass({
     )
     if (user != null) {
       button = (
-        <a>
-          <semantic.Image size='mini' shape='rounded' src={user.avatar_url} />
-        </a>
+        <semantic.Popup
+          trigger={
+            <a>
+              <semantic.Image size='mini' shape='rounded' src={user.avatar_url} />
+            </a>
+          }
+          on='click'
+        >
+          <semantic.Menu
+            vertical
+          >
+            <semantic.Form method='post' action='/gitlab/users/sign_out'>
+              <semantic.Form.Input  type='hidden'  name='_method' value='delete' />
+              <semantic.Form.Input  type='hidden'  name='authenticity_token' value={this.state.logoutToken} />
+              <semantic.Form.Input type='submit'   name='commit'             value='Sign out' />
+            </semantic.Form>
+          </semantic.Menu>
+        </semantic.Popup>
       )
     }
     return (
