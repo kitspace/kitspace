33504be775604dbd1e0c4047dc1110dd4b719201
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-04-11 15:17:06 -0300

    Remove login page

--- src/login/render_login.jsx
+++
@@ -1,8 +0,0 @@
-const React    = require('react')
-const ReactDOM = require('react-dom')
-const Login    = require('./login')
-
-ReactDOM.render(
-  <Login />,
-  document.getElementById('content')
-)
