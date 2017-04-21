2193a810bbee3b11e6507222357776d356e5d888
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-04-21 17:00:32 +0200

    Clean up and refactor

--- src/settings/settings.jsx
+++ src/settings/settings.jsx
@@ -13,33 +13,20 @@ const TitleBar = require('../title_bar')
 const Settings = React.createClass({
   getInitialState() {
     return {
-      user: null,
-      emails: null,
-      token: '',
     }
   },
   componentWillMount() {
-    superagent.get('/gitlab/api/v4/user')
-      .set('Accept', 'application/json')
-      .withCredentials()
-      .then(r => this.setState({user: r.body}))
-      .catch(e => this.setState({user: 'not signed in'}))
     superagent.get('/gitlab/profile')
       .withCredentials()
       .then(r => {
-        const doc   = (new DOMParser).parseFromString(r.text, 'text/html')
-        const token = doc.querySelector('input[name=authenticity_token]').value
-        const email = doc.querySelector('input[name="user[email]"]').value
-        const name  = doc.querySelector('input[name="user[name]"]').value
-        document.querySelector('input[name=authenticity_token]').value = token
-        document.querySelector('input[name="user[email]"]').value      = email
-        document.querySelector('input[name="user[name]"]').value       = name
+        const doc = (new DOMParser).parseFromString(r.text, 'text/html')
+        function copy(selector) {
+          document.querySelector(selector).value = doc.querySelector(selector).value
+        }
+        copy('input[name=authenticity_token]')
+        copy('input[name="user[email]"]')
+        copy('input[name="user[name]"]')
       }).catch(e => console.error(e))
-    superagent.get('/gitlab/api/v4/user/emails')
-      .set('Accept', 'application/json')
-      .withCredentials()
-      .then(r => this.setState({emails: r.body}))
-      .catch(e => this.setState({emails: 'error'}))
     //set the state to loading if it hasn't gotten the user info after a second
     setTimeout(() => {
       if (this.state.user == null) {
@@ -47,40 +34,29 @@ const Settings = React.createClass({
       }
     }, 1000)
   },
-  submitForm(event) {
-    event.preventDefault()
-    const email = document.querySelector('input[name=email]').value
-    superagent.post('/gitlab/api/v4/user/emails')
-      .withCredentials()
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
-      .send({email})
-      .then(r => console.log(r.body))
-      .catch(e => console.log(e))
-  },
   render() {
     const user = this.state.user || {}
     return (
-      <div>
-        <TitleBar user={this.state.user}>
+      <div className='Settings'>
+        <TitleBar>
           <div className='titleText'>
             {'Settings'}
           </div>
         </TitleBar>
         <semantic.Container>
-          <pre>
-            {(this.state.user || {}).email}
-          </pre>
-          <pre>
-            {JSON.stringify(this.state.emails, null, 2)}
-          </pre>
           <semantic.Form encType="multipart/form-data" action="/gitlab/profile" acceptCharset="UTF-8" method="post">
             <input name="utf8" type="hidden" value="✓" />
             <input type="hidden" name="_method" value="put" />
             <input name='authenticity_token' type='hidden' />
-            <semantic.Form.Input name='user[name]' type='text' />
-            <semantic.Form.Input name='user[email]' type='text'/>
-            <semantic.Button type='submit'>{'Save'}</semantic.Button>
+            <semantic.Grid>
+              <semantic.Grid.Column width={6}>
+                <label>Name</label>
+                <semantic.Form.Input name='user[name]' type='text' />
+                <label>Email</label>
+                <semantic.Form.Input name='user[email]' type='text'/>
+                <semantic.Button type='submit'>{'Save'}</semantic.Button>
+              </semantic.Grid.Column>
+            </semantic.Grid>
           </semantic.Form>
         </semantic.Container>
       </div>
