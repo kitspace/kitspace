87df3d2c460d88e30d16f723f9bed33a06078cf6
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-04-21 20:05:43 +0200

    Clean up and make email message work

--- src/settings/settings.jsx
+++ src/settings/settings.jsx
@@ -1,18 +1,16 @@
-const Redux      = require('redux')
-const React      = require('react')
-const {h}        = require('react-hyperscript-helpers')
-const path       = require('path')
-const immutable  = require('immutable')
-const superagent = require('superagent')
-const semantic   = require('semantic-ui-react')
-const {Router, Route, Link, hashHistory} = require('react-router')
+const Redux       = require('redux')
+const React       = require('react')
+const superagent  = require('superagent')
+const semantic    = require('semantic-ui-react')
+const htmlToReact = new (new require('html-to-react')).Parser(React).parse
 
 const TitleBar = require('../title_bar')
 
-
+const defaultMessage = 'We also use email for avatar detection if no avatar is uploaded.'
 const Settings = React.createClass({
   getInitialState() {
     return {
+      emailMessage: ''
     }
   },
   componentWillMount() {
@@ -26,16 +24,13 @@ const Settings = React.createClass({
         copy('input[name=authenticity_token]')
         copy('input[name="user[email]"]')
         copy('input[name="user[name]"]')
+        const emailMessage = doc.querySelector('input[name="user[email]"]').nextElementSibling.innerHTML
+        this.setState({emailMessage})
       }).catch(e => console.error(e))
-    //set the state to loading if it hasn't gotten the user info after a second
-    setTimeout(() => {
-      if (this.state.user == null) {
-        this.setState({user: 'loading'})
-      }
-    }, 1000)
   },
   render() {
     const user = this.state.user || {}
+    const emailWarning = this.state.emailMessage !== defaultMessage
     return (
       <div className='Settings'>
         <TitleBar>
@@ -44,16 +39,24 @@ const Settings = React.createClass({
           </div>
         </TitleBar>
         <semantic.Container>
-          <semantic.Form encType="multipart/form-data" action="/gitlab/profile" acceptCharset="UTF-8" method="post">
+          <semantic.Form
+            warning={emailWarning && this.state.emailMessage !== ''}
+            encType="multipart/form-data" action="/gitlab/profile"
+            acceptCharset="UTF-8"
+            method="post"
+          >
             <input name="utf8" type="hidden" value="✓" />
             <input type="hidden" name="_method" value="put" />
             <input name='authenticity_token' type='hidden' />
             <semantic.Grid>
-              <semantic.Grid.Column width={6}>
+              <semantic.Grid.Column mobile={14} tablet={10} computer={8}>
                 <label>Name</label>
                 <semantic.Form.Input name='user[name]' type='text' />
                 <label>Email</label>
                 <semantic.Form.Input name='user[email]' type='text'/>
+                <semantic.Message warning={emailWarning} id='emailMessage'>
+                  {htmlToReact(`<div>${this.state.emailMessage}</div>`)}
+                </semantic.Message>
                 <semantic.Button type='submit'>{'Save'}</semantic.Button>
               </semantic.Grid.Column>
             </semantic.Grid>
