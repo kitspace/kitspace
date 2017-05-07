edb03f4fc7c1462bf85d4f1e4a26bca25b5a4b33
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-05-07 02:15:33 +0100

    Re-add media_queries in js

---
+++ src/media_queries.js
@@ -0,0 +1,6 @@
+//these are also defined in common.scss and need to be adjusted there and here
+const mobile             = '(max-width: 458px)'
+const small_width        = '(max-width: 949px)'
+const semantic_container = '(max-width: 1199px)'
+
+module.exports = {mobile, semantic_container, small_width}
