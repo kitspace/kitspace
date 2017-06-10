0db50e007e80334c610f6d22ae7bb6695c41d3a6
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-06-10 17:57:04 +0100

    Fix js media queries and make header cells wrap

--- src/media_queries.js
+++ src/media_queries.js
@@ -1,6 +1,6 @@
 //these are also defined in common.scss and need to be adjusted there and here
-const small_width        = '(max-width: 768)'
-const semantic_container = '(max-width: 1199)'
+const small_width        = '(max-width: 768px)'
+const semantic_container = '(max-width: 1199px)'
 const mobile_m           = '(max-width: 458px)'
 
 const mobile = `
