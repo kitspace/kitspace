727804b5c0aa18166812709bacf5ebe422e6f233
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-11-10 01:30:51 +0000

    Begin test for top-large.png

--- services/board-files/src/app.js
+++ services/board-files/src/app.js
@@ -1,7 +1,8 @@
 const app = require('express')()
 
-app.get("/gerbers/:projectId/:sha/images/:fileName", (req, res) => {
+app.get("/board-files/:projectId/:sha/images/:fileName", (req, res) => {
   const {projectId, sha, fileName} = req.params
+  res.send()
 })
 
 
--- services/board-files/test/test_app.js
+++ services/board-files/test/test_app.js
@@ -1,13 +1,21 @@
 const assert = require('assert')
+const supertest = require('supertest')
+
 const app = require('../src/app')
 const GitlabClient = require('../../../modules/gitlab-client')
 
-const client = new GitlabClient('https://gitlab2.kitnic.it/accounts')
+const gitlab = new GitlabClient('https://gitlab2.kitnic.it/accounts')
 
 describe('app', () => {
     it('serves top-large.png', async () => {
-        const projects = await client.getProjects()
+        const projects = await gitlab.getProjects()
         const id = projects[0].id
-        assert(false)
+        const sha = gitlab.getProjectHead(id)
+        const png = await supertest(app)
+            .get(`/board-files/${id}/${sha}/images/top-large.png`)
+            .expect(200)
+            .then(r => r.text)
+        console.log(png)
+        assert(png)
     })
 })
