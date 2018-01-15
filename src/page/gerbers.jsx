0d0c85e09a094310771629f399524d7e7f50c3d9
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-01-15 17:13:08 +0000

    Start aisler and pcbway buttons

--- src/page/gerbers.jsx
+++ src/page/gerbers.jsx
@@ -1,17 +1,29 @@
-'use strict'
 const React = require('react')
 const zipPath = require('../zip-info.json')
+const semantic = require('semantic-ui-react')
 
 let Gerbers = React.createClass({
-  render: function() {
+  render() {
     return (
-      <div className="gerbersContainer">
-        <a className="zipPath" href={zipPath}>
-          <div className="gerbers">
-            <i className="octicon octicon-circuit-board" /> Download Gerbers
-          </div>
-        </a>
-      </div>
+      <semantic.Segment className="gerbersContainer">
+        <semantic.Label color="green" ribbon="top">
+          Order PCBs:
+        </semantic.Label>
+        <semantic.Button.Group>
+          <semantic.Button basic>
+            <a className="zipPath" href={zipPath}>
+              <semantic.Icon name="download" />
+              Download
+            </a>
+          </semantic.Button>
+          <semantic.Button basic>
+            <img src="/images/aisler.png" />
+          </semantic.Button>
+          <semantic.Button basic>
+            <img src="/images/pcbway.png" />
+          </semantic.Button>
+        </semantic.Button.Group>
+      </semantic.Segment>
     )
   }
 })
