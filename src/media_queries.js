03ab6c765648aa44a21710ece91f4ab4d99e0fb0
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-01-15 22:48:38 +0000

    Make PCB button stack on mobile

--- src/media_queries.js
+++ src/media_queries.js
@@ -1,7 +1,8 @@
-//these are also defined in common.scss and need to be adjusted there and here
+//these are also defined in common.scss if adjustments need to be made, they
+//need to be made there and here
 const small_width        = '(max-width: 768px)'
 const semantic_container = '(max-width: 1199px)'
-const mobile_m           = '(max-width: 458px)'
+const mobile_m           = '(max-width: 499px)'
 
 const mobile = `
   (orientation: portrait) and (max-device-width: 480px)
--- src/page/gerbers.jsx
+++ src/page/gerbers.jsx
@@ -1,40 +1,48 @@
 const React = require('react')
+const semantic = require('semantic-ui-react')
+const ReactResponsive = require('react-responsive')
+
+const mediaQueries = require('../media_queries')
 const zipPath = require('../zip-info.json')
 const info = require('../info.json')
-const semantic = require('semantic-ui-react')
+
 const zipUrl = `https://kitspace.org/boards/${info.id}/${zipPath}`
 
 let Gerbers = React.createClass({
   render() {
     return (
-      <div className="gerbersContainer">
-        <semantic.Menu compact borderless>
-          <semantic.Menu.Item>
-            <h4>Order PCBs:</h4>
-          </semantic.Menu.Item>
-          <semantic.Menu.Item>
-            <a className="zipPath" href={zipPath}>
-              <semantic.Icon name="download" />
-              Download
-            </a>
-          </semantic.Menu.Item>
-          <semantic.Menu.Item>
-            <a href={`https://aisler.net/p/new?url=${zipUrl}&ref=kitspace`}>
-              <img style={{minWidth: 100}} src="/images/aisler.png" />
-            </a>
-          </semantic.Menu.Item>
-          <semantic.Menu.Item>
-            <a
-              href={
-                'https://www.pcbway.com/orderonline.aspx' +
-                `?url=${zipUrl}&from=kitspace`
-              }
-            >
-              <img style={{minWidth: 100}} src="/images/pcbway.png" />
-            </a>
-          </semantic.Menu.Item>
-        </semantic.Menu>
-      </div>
+      <ReactResponsive query={mediaQueries.mobile_m}>
+        {matches => (
+          <div className="gerbersContainer">
+            <semantic.Menu compact stackable={matches} borderless>
+              <semantic.Menu.Item>
+                <h4>Order PCBs:</h4>
+              </semantic.Menu.Item>
+              <semantic.Menu.Item>
+                <a className="zipPath" href={zipPath}>
+                  <semantic.Icon name="download" />
+                  Download
+                </a>
+              </semantic.Menu.Item>
+              <semantic.Menu.Item>
+                <a href={`https://aisler.net/p/new?url=${zipUrl}&ref=kitspace`}>
+                  <img style={{minWidth: 100}} src="/images/aisler.png" />
+                </a>
+              </semantic.Menu.Item>
+              <semantic.Menu.Item>
+                <a
+                  href={
+                    'https://www.pcbway.com/orderonline.aspx' +
+                    `?url=${zipUrl}&from=kitspace`
+                  }
+                >
+                  <img style={{minWidth: 100}} src="/images/pcbway.png" />
+                </a>
+              </semantic.Menu.Item>
+            </semantic.Menu>
+          </div>
+        )}
+      </ReactResponsive>
     )
   }
 })
