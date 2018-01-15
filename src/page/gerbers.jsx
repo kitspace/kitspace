19cc1a5ad5ac7f7fbcce3df626c7799b1a590389
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-01-15 17:48:02 +0000

    Improve PCB buttons

--- src/page/gerbers.jsx
+++ src/page/gerbers.jsx
@@ -1,29 +1,40 @@
 const React = require('react')
 const zipPath = require('../zip-info.json')
+const info = require('../info.json')
 const semantic = require('semantic-ui-react')
+const zipUrl = `https://kitspace.org/boards/${info.id}/${zipPath}`
 
 let Gerbers = React.createClass({
   render() {
     return (
-      <semantic.Segment className="gerbersContainer">
-        <semantic.Label color="green" ribbon="top">
-          Order PCBs:
-        </semantic.Label>
-        <semantic.Button.Group>
-          <semantic.Button basic>
+      <div className="gerbersContainer">
+        <semantic.Menu compact borderless>
+          <semantic.Menu.Item>
+            <h4>Order PCBs:</h4>
+          </semantic.Menu.Item>
+          <semantic.Menu.Item>
             <a className="zipPath" href={zipPath}>
               <semantic.Icon name="download" />
               Download
             </a>
-          </semantic.Button>
-          <semantic.Button basic>
-            <img src="/images/aisler.png" />
-          </semantic.Button>
-          <semantic.Button basic>
-            <img src="/images/pcbway.png" />
-          </semantic.Button>
-        </semantic.Button.Group>
-      </semantic.Segment>
+          </semantic.Menu.Item>
+          <semantic.Menu.Item>
+            <a href={`https://aisler.net/p/new?url=${zipUrl}&ref=kitspace`}>
+              <img style={{minWidth: 100}} src="/images/aisler.png" />
+            </a>
+          </semantic.Menu.Item>
+          <semantic.Menu.Item>
+            <a
+              href={
+                'https://www.pcbway.com/orderonline.aspx' +
+                `?url=${zipUrl}&from=kitspace`
+              }
+            >
+              <img style={{minWidth: 100}} src="/images/pcbway.png" />
+            </a>
+          </semantic.Menu.Item>
+        </semantic.Menu>
+      </div>
     )
   }
 })
