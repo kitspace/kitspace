925f0cef09318eb0b22a1bd48cc611b8b1693f19
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-01-28 22:11:35 +0000

    Test out login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,28 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const Gitlab = require('kitspace-gitlab-client')
-const gitlab = new Gitlab('http://localhost:8079')
+const gitlab = new Gitlab('http://localhost:8080/gitlab')
+const superagent = require('superagent')
 
 class Index extends React.Component {
   constructor() {
     super()
-    this.state = {projects: []}
+    this.state = {projects: [], user: null}
   }
   componentDidMount() {
     gitlab.getProjects().then(projects => {
-      console.log(projects)
       this.setState({projects})
     })
+    superagent.get('http://localhost:8080/gitlab/api/v4/user')
+      .set('Accept', 'application/json')
+      .withCredentials()
+      .then(r => this.setState({user: r.body}))
+      .catch(e => this.setState({user: 'not signed in'}))
   }
   render() {
     return (
       <div>
-        <pre>{JSON.stringify(this.state.projects, null, 2)}</pre>
+        <pre>{JSON.stringify(this.state.user, null, 2)}</pre>
       </div>
     )
   }
