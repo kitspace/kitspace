bb127e8925d912b4d2db8b0f84163e4fce57d862
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-03-23 21:13:32 +0000

    login: implement token getting

--- services/login/src/app.js
+++ services/login/src/app.js
@@ -1,7 +1,14 @@
 const app = require('express')()
+const cheerio = require('cheerio')
+//const gitlab = new Gitlab('http://localhost:8080/gitlab')
+const superagent = require('superagent')
 
 app.get('/login', (req, res) => {
-    res.send('hello')
+  superagent.get('http://localhost:8080/gitlab/users/sign_in').then(r => {
+    const $ = cheerio.load(r.text)
+    const token = $('input[name=authenticity_token]').attr('value')
+    res.send(token)
+  })
 })
 
 module.exports = app
