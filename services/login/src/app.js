7b78249c5bd7ff8aeee9dc08b2871ff551dcaa90
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-06-24 15:56:05 +0100

    Revert "login: Use gitlab internal port"
    This reverts commit fb2b52e664718dfd5242e546205cd0e32602e392.

--- services/login/src/app.js
+++ services/login/src/app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 const cheerio = require('cheerio')
 const bodyParser = require('body-parser')
-//const gitlab = new Gitlab('http://localhost:8080/gitlab')
+//const gitlab = new Gitlab('http://localhost:7334/gitlab')
 const superagent = require('superagent')
 const cookieParser = require('cookie-parser')
 
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser())
 
 app.get('/', (req, res) => {
-  const p = superagent.get('http://localhost:8080/gitlab/users/sign_in')
+  const p = superagent.get('http://localhost:7334/gitlab/users/sign_in')
   if (req.cookies._gitlab_session) {
     p.set('cookie', `_gitlab_session=${req.cookies._gitlab_session}`)
   }
@@ -28,7 +28,7 @@ app.get('//github', (req, res) => {
 
 app.post('//github', (req, res) => {
   const p = superagent
-    .post('http://localhost:8080/gitlab/users/auth/github')
+    .post('http://localhost:7334/gitlab/users/auth/github')
     .redirects(0)
     .send(`authenticity_token=${encodeURIComponent(req.body.authenticity_token)}`)
   if (req.cookies._gitlab_session) {
@@ -49,7 +49,7 @@ app.post('//github', (req, res) => {
 
 app.get('//sign_out', (req, res) => {
   const p = superagent
-    .get('http://localhost:8080/gitlab/users/sign_out')
+    .get('http://localhost:7334/gitlab/users/sign_out')
     .send(`authenticity_token=${encodeURIComponent(req.body.authenticity_token)}`)
   if (req.cookies._gitlab_session) {
     p.set('cookie', `_gitlab_session=${req.cookies._gitlab_session}`)
@@ -60,7 +60,7 @@ app.get('//sign_out', (req, res) => {
 
 app.post('/', (req, res) => {
   const p = superagent
-    .post('http://localhost:8080/gitlab/users/sign_in')
+    .post('http://localhost:7334/gitlab/users/sign_in')
     .redirects(0)
     .send(`authenticity_token=${encodeURIComponent(req.body.authenticity_token)}`)
     .send(`user[login]=${req.body['user[login]']}`)
