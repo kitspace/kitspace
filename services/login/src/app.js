aeb34ef19bc42e65c3d09448e9d94dcd8d44e125
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-06-25 18:05:37 +0100

    Change gitlab path to !gitlab

--- services/login/src/app.js
+++ services/login/src/app.js
@@ -1,20 +1,19 @@
 const app = require('express')()
 const cheerio = require('cheerio')
 const bodyParser = require('body-parser')
-//const gitlab = new Gitlab('${DOMAIN}:${PORT}/gitlab')
 const superagent = require('superagent')
 const cookieParser = require('cookie-parser')
 
 require('dotenv').config({path: '../../.env'})
 
-const {DOMAIN, PORT} = process.env
+const {DOMAIN, PORT, GITLAB_PATH} = process.env
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser())
 
 
 app.get('/', (req, res) => {
-  const p = superagent.get(`${DOMAIN}:${PORT}/gitlab/users/sign_in`)
+  const p = superagent.get(`${DOMAIN}:${PORT}/${GITLAB_PATH}/users/sign_in`)
   if (req.cookies._gitlab_session) {
     p.set('cookie', `_gitlab_session=${req.cookies._gitlab_session}`)
   }
@@ -33,7 +32,7 @@ app.get('//github', (req, res) => {
 
 app.post('//github', (req, res) => {
   const p = superagent
-    .post(`${DOMAIN}:${PORT}/gitlab/users/auth/github`)
+    .post(`${DOMAIN}:${PORT}/${GITLAB_PATH}/users/auth/github`)
     .redirects(0)
     .send(`authenticity_token=${encodeURIComponent(req.body.authenticity_token)}`)
   if (req.cookies._gitlab_session) {
@@ -54,7 +53,7 @@ app.post('//github', (req, res) => {
 
 app.get('//sign_out', (req, res) => {
   const p = superagent
-    .get(`${DOMAIN}:${PORT}/gitlab/users/sign_out`)
+    .get(`${DOMAIN}:${PORT}/${GITLAB_PATH}/users/sign_out`)
     .send(`authenticity_token=${encodeURIComponent(req.body.authenticity_token)}`)
   if (req.cookies._gitlab_session) {
     p.set('cookie', `_gitlab_session=${req.cookies._gitlab_session}`)
@@ -65,7 +64,7 @@ app.get('//sign_out', (req, res) => {
 
 app.post('/', (req, res) => {
   const p = superagent
-    .post(`${DOMAIN}:${PORT}/gitlab/users/sign_in`)
+    .post(`${DOMAIN}:${PORT}/${GITLAB_PATH}/users/sign_in`)
     .redirects(0)
     .send(`authenticity_token=${encodeURIComponent(req.body.authenticity_token)}`)
     .send(`user[login]=${req.body['user[login]']}`)
