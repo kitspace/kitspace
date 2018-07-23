860d49e2b492b2956ed24f8660c748e6e08ef14a
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-07-23 21:44:35 +0100

    frontend: Show user in index

--- services/frontend/pages/index.js
+++ services/frontend/pages/index.js
@@ -2,12 +2,14 @@ import Link from 'next/link'
 import React from 'react'
 import Gitlab from 'kitspace-gitlab-client'
 
-const gitlab = new Gitlab(
-  process.env.KITSPACE_DOMAIN + ':' + process.env.KITSPACE_PORT + '/!gitlab',
-)
-
 export default class Index extends React.Component {
-  static async getInitialProps() {
+  static async getInitialProps({req}) {
+    const cookie = req ? req.headers.cookie : null
+    const gitlab = new Gitlab(
+      process.env.KITSPACE_DOMAIN + ':' + process.env.KITSPACE_PORT + '/!gitlab',
+      null,
+      cookie,
+    )
     const [projects, user] = await Promise.all([
       gitlab.getProjects(),
       gitlab.getCurrentUser(),
@@ -15,14 +17,16 @@ export default class Index extends React.Component {
     return {projects, user}
   }
   render() {
-    return this.props.projects.map(project => {
-      return (
-        <li>
-          <Link href={`/${project.path_with_namespace}`}>
-            <a>{project.path_with_namespace}</a>
-          </Link>
-        </li>
-      )
-    })
+    return [<pre>{(this.props.user || {}).username}</pre>].concat(
+      this.props.projects.map(project => {
+        return (
+          <li>
+            <Link href={`/${project.path_with_namespace}`}>
+              <a>{project.path_with_namespace}</a>
+            </Link>
+          </li>
+        )
+      }),
+    )
   }
 }
