bef4fe65ef82dd2aec9cc35dbfb80f10d5b4dac8
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-07-31 15:00:03 +0100

    frontend: Add TitleBar to /login

--- services/frontend/pages/login.js
+++ services/frontend/pages/login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import superagent from 'superagent'
 import Gitlab from 'kitspace-gitlab-client'
+import TitleBar from '../components/TitleBar'
 
 class Login extends React.Component {
   static async getInitialProps({req}) {
@@ -24,57 +25,62 @@ class Login extends React.Component {
   }
   render() {
     return (
-      <div>
-        <pre>{JSON.stringify(this.props.user, null, 2)}</pre>
-        <input
-          onChange={e => this.setState({username: e.target.value})}
-          id="username"
-          name="username"
-          value={this.state.username}
-        />
-        <input
-          onChange={e => {
-            this.setState({password: e.target.value})
-          }}
-          type="password"
-          id="password"
-          name="password"
-          value={this.state.password}
-        />
-        <button
-          onClick={() => {
-            const {username, password, authenticity_token} = this.state
-            superagent
-              .post('/!login/api')
-              .send(`authenticity_token=${encodeURIComponent(authenticity_token)}`)
-              .send(`user[login]=${encodeURIComponent(username)}`)
-              .send(`user[password]=${encodeURIComponent(password)}`)
-              .send('user[remember_me]=0')
-              .send('utf8=✓')
-              .catch(e => console.error(e))
-          }}
-        >
-          ajax login
-        </button>
-        <pre>{this.state.authenticity_token}</pre>
-
-        <form
-          action="/!gitlab/users/auth/github"
-          method="post"
-          onSubmit={e => {
-            if (referrer) {
-              setCookie('oauthLoginRedirect', referrer)
-            }
-          }}
-        >
+      <>
+        <TitleBar user={this.props.user} />
+        <div>
+          <pre>{JSON.stringify(this.props.user, null, 2)}</pre>
           <input
-            type="hidden"
-            name="authenticity_token"
-            value={this.state.authenticity_token}
+            onChange={e => this.setState({username: e.target.value})}
+            id="username"
+            name="username"
+            value={this.state.username}
           />
-          <input type="submit" value="Github" />
-        </form>
-      </div>
+          <input
+            onChange={e => {
+              this.setState({password: e.target.value})
+            }}
+            type="password"
+            id="password"
+            name="password"
+            value={this.state.password}
+          />
+          <button
+            onClick={() => {
+              const {username, password, authenticity_token} = this.state
+              superagent
+                .post('/!login/api')
+                .send(
+                  `authenticity_token=${encodeURIComponent(authenticity_token)}`,
+                )
+                .send(`user[login]=${encodeURIComponent(username)}`)
+                .send(`user[password]=${encodeURIComponent(password)}`)
+                .send('user[remember_me]=0')
+                .send('utf8=✓')
+                .catch(e => console.error(e))
+            }}
+          >
+            ajax login
+          </button>
+          <pre>{this.state.authenticity_token}</pre>
+
+          <form
+            action="/!gitlab/users/auth/github"
+            method="post"
+            onSubmit={e => {
+              if (referrer) {
+                setCookie('oauthLoginRedirect', referrer)
+              }
+            }}
+          >
+            <input
+              type="hidden"
+              name="authenticity_token"
+              value={this.state.authenticity_token}
+            />
+            <input type="submit" value="Github" />
+          </form>
+        </div>
+      </>
     )
   }
 }
