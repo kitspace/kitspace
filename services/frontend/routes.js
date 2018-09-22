316fb7280fedc8bb3904d666dd2762efa807899a
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-09-23 00:32:25 +0100

    frontend: Remove router hook

--- services/frontend/routes.js
+++ services/frontend/routes.js
@@ -2,8 +2,4 @@ const Routes = require('next-routes')
 
 const routes = Routes().add('/:namespace/:projectname', 'project')
 
-routes.Router.onRouteChangeStart = url => {
-  console.log('App is changing to: ', url)
-}
-
 module.exports = routes
