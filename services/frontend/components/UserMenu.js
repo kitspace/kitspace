cb60d52a0cac9b4ea5c633ebe5fe96f4fe5ca991
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-09-22 22:47:51 +0100

    frontend: Improve logout

--- services/frontend/components/UserMenu.js
+++ services/frontend/components/UserMenu.js
@@ -1,27 +1,37 @@
-import './UserMenu.scss'
 import React from 'react'
 import * as semantic from 'semantic-ui-react'
-import Link from 'next/link'
+import {withRouter} from 'next/router'
+import superagent from 'superagent'
 
-function UserMenu(props) {
-  const user = props.user || {}
-  return (
-    <div className="UserMenu">
-      <div className="userName">{user.username}</div>
-      <semantic.Menu vertical attached>
-        <Link prefetch href="/settings">
-        <semantic.Menu.Item>
-          <semantic.Icon name="settings" />
-          Settings
-        </semantic.Menu.Item>
-      </Link>
-        <semantic.Menu.Item href="/!gitlab/users/sign_out">
-          <semantic.Icon name="sign out" />
-          Sign out
-        </semantic.Menu.Item>
-      </semantic.Menu>
-    </div>
-  )
+import {Link} from '../routes'
+
+import './UserMenu.scss'
+
+class UserMenu extends React.Component {
+  logout = () =>
+    superagent
+      .get('/!gitlab/users/sign_out')
+      .then(r => this.props.router.push('/login'))
+  render() {
+    const user = this.props.user || {}
+    return (
+      <div className="UserMenu">
+        <div className="userName">{user.username}</div>
+        <semantic.Menu vertical attached>
+          <Link prefetch href="/settings">
+            <semantic.Menu.Item>
+              <semantic.Icon name="settings" />
+              Settings
+            </semantic.Menu.Item>
+          </Link>
+          <semantic.Menu.Item onClick={this.logout}>
+            <semantic.Icon name="sign out" />
+            Log out
+          </semantic.Menu.Item>
+        </semantic.Menu>
+      </div>
+    )
+  }
 }
 
-export default UserMenu
+export default withRouter(UserMenu)
