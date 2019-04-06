30cc250ad420d0b2ef6e34995cf8d655977ec217
ollyoid <ochild242@gmail.com>
2019-04-06 15:22:22 +0100

    Start opt in comms

--- src/opt_in.jsx
+++ src/opt_in.jsx
@@ -1,7 +1,18 @@
 const React = require('react')
 const semantic = require('semantic-ui-react')
+const superagent = require('superagent')
 
 class OptIn extends React.Component {
+  constructor(...args) {
+    super(...args)
+    this.state = {optOut: null}
+  }
+  componentDidMount() {
+    const promise = superagent.get(
+      'https://matomo.kitspace.org/index.php?module=API&method=AjaxOptOut.isTracked'
+    )
+    promise.then(r => console.log(r))
+  }
   render() {
     return (
       <semantic.Segment compact className="OptIn">
