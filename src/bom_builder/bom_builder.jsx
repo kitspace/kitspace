341695eb8c05b5e12969a32e892dc1e8ed3197df
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-08-03 16:58:11 +0100

    Tweak bom_builder video embeds for mobile

--- src/bom_builder/bom_builder.jsx
+++ src/bom_builder/bom_builder.jsx
@@ -7,15 +7,17 @@ const TitleBar = require('../title_bar')
 const text = `
 # The Kitspace BOM Builder
 
-The BOM Builder allows you to automatically find in-stock components and alternatives across distributors and lets you add entire bill of materials directly to shopping carts by connecting up to [1-click BOM](https://1clickbom.com). In our experience this can cut the purchasing time down from a few hours to a few minutes. We are now in a closed beta phase so if you are interested in trying it out please [get in touch](mailto:info@kitspace.org?subject=BOM%20Builder).
+The BOM Builder allows you to automatically find in-stock components and alternatives across distributors and lets you add entire bill of materials directly to shopping carts by connecting up to [1-click BOM](https://1clickbom.com).
+
+In our experience this can cut the purchasing time down from a few hours to a few minutes. We are now in a closed beta phase so if you are interested in trying it out please [get in touch](mailto:info@kitspace.org?subject=BOM%20Builder).
 
 ## A quick demo of the alpha version
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/m96G7B1doRQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe style="max-width:90%" width="560" height="315" src="https://www.youtube.com/embed/m96G7B1doRQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 ## A demo of all the features of the beta version
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/U7GB7RV1VzE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe style="max-width:90%" width="560" height="315" src="https://www.youtube.com/embed/U7GB7RV1VzE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 `
 
