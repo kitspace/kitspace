d7ca107d6a77dace174b48d24280bf035ebda75f
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-08-03 12:52:42 +0100

    Shorten bom builder text

--- src/bom_builder/bom_builder.jsx
+++ src/bom_builder/bom_builder.jsx
@@ -7,17 +7,13 @@ const TitleBar = require('../title_bar')
 const text = `
 # The Kitspace BOM Builder
 
-The BOM Builder allows you to automatically find in-stock components and alternatives across distributors. It also lets you add entire bill of materials directly to shopping carts by connecting up to [1-click BOM](https://1clickbom.com).
+The BOM Builder allows you to automatically find in-stock components and alternatives across distributors. It also lets you add entire bill of materials directly to shopping carts by connecting up to [1-click BOM](https://1clickbom.com). We are now in a closed beta phase so if you are interested in trying it out please [get in touch](mailto:info@kitspace.org?subject=BOM%20Builder).
 
-You can easily select alternatives for line items even for generic components such as resistors and capacitors. The aim is to cut the purchasing time down from several hours to a few minutes and quickly buy alternatives when parts go out of stock at your preferred distributor.
-
-We are now in a closed beta phase so if you are interested in trying it out please [get in touch](mailto:info@kitspace.org?subject=BOM%20Builder).
-
-A quick demo of the alpha version:
+## A quick demo of the alpha version
 
 <iframe width="560" height="315" src="https://www.youtube.com/embed/m96G7B1doRQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-A longer video going through all of the features of the beta version:
+## A demo of all the features of the beta version
 
 <iframe width="560" height="315" src="https://www.youtube.com/embed/U7GB7RV1VzE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
