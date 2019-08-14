47a4913ecef555ee859298de9801976792c98a03
ollyoid <ochild242@gmail.com>
2019-08-14 22:36:16 +0100

    Add tracespace external link icon

--- src/page/board_showcase.jsx
+++ src/page/board_showcase.jsx
@@ -33,43 +33,44 @@ var BoardShowcase = React.createClass({
 	    backBoardClass += ' selectedBoard'
 	}
 	const children = this.props.children || [<div />, <div />]
-	const TraceSpaceLink = props => zipUrl ? <a href={`https://tracespace.io/view/?boardUrl=${zipUrl}`} target="_blank">{props.children}</a> : <div>{props.children}</div>
+	const TraceSpaceLink = props => zipUrl ? <a href={`https://tracespace.io/view/?boardUrl=${zipUrl}`} target="_blank" title="Open in Tracespace">{props.children}</a> : <div>{props.children}</div>
 	return (
 	    <div className="boardShowcaseContainer">
-              <div className="toggleBoardView responsiveTabs">
-		<button
-		  disabled={this.state.viewFrontBoard}
-		  className="circuitToggleBtn circuitFrontBtn"
-		  onClick={this.frontBoardView}
-		  >
-		  Front
-		</button>
-		<button
-		  disabled={!this.state.viewFrontBoard}
-		  className="circuitToggleBtn circuitBackBtn"
-		  onClick={this.backBoardView}
-		  >
-		  Back
-		</button>
-              </div>
-	       <TraceSpaceLink>
-              <div className="boardShowcase">
-		<LazyLoad
-		  once={true}
-		  component={React.createElement('div', {className: 'img'})}
-		  distance={300}
-		  >
-		  <div className="boardContainer">
-		    <div className={frontBoardClass}>{children[0]}</div>
-		    <div className="circuitBorderContainer">
-                      <div className="circuitBorder" />
-		    </div>
-		    <div className={backBoardClass}>{children[1]}</div>
-		  </div>
-		</LazyLoad>
-		<Icon name="external alternate"/>
+            <div className="toggleBoardView responsiveTabs">
+				<button
+		  		disabled={this.state.viewFrontBoard}
+		  		className="circuitToggleBtn circuitFrontBtn"
+		  		onClick={this.frontBoardView}>
+		  			Front
+				</button>
+				<button
+				disabled={!this.state.viewFrontBoard}
+				className="circuitToggleBtn circuitBackBtn"
+				onClick={this.backBoardView}
+				>
+		  			Back
+				</button>
+            </div>
+	       	<TraceSpaceLink>
+            	<div className="boardShowcase">
+					<LazyLoad
+		 			once={true}
+		  			component={React.createElement('div', {className: 'img'})}
+		  			distance={300}
+		  			>
+		  				<div className="boardContainer">
+		    				<div className={frontBoardClass}>{children[0]}</div>
+		    				<div className="circuitBorderContainer">
+                      			<div className="circuitBorder" />
+		    				</div>
+		    				<div className={backBoardClass}>{children[1]}</div>
+		  				</div>
+					</LazyLoad>
+					<Icon id="externalLink" name="external alternate"/>
               </div>
 	      </TraceSpaceLink>
+		  
+
 	    </div>
 	)
     }
