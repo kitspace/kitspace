4bdfc5f6c48cd96ebe8dd1d30e52a78ce8f9e200
sdbowen <sdbowen.git@gmail.com>
2019-10-23 14:11:46 +0200

    Add PCB Shopper link

--- src/page/order_pcbs.jsx
+++ src/page/order_pcbs.jsx
@@ -13,46 +13,85 @@ const pcbwayUrl = `https://www.pcbway.com/QuickOrderOnline.aspx?fileurl=${zipUrl
 
 const royalCircuitsUrl = 'https://www.royalcircuits.com/'
 
+const pcbShopperUrl = `https://pcbshopper.com/?Width=${width}&Height=${height}&Units=mm&GetPrices`
+
 let OrderPcbs = React.createClass({
   render() {
     return (
       <ReactResponsive query={'(max-width: 710px)'}>
         {matches => (
-          <div className="OrderPcbs">
-            <semantic.Menu stackable={matches} compact collapsing borderless>
-              <semantic.Menu.Item>
-                <h4>Order PCBs:</h4>
-              </semantic.Menu.Item>
-              <semantic.Menu.Item as="a" href={zipPath}>
-                <semantic.Icon name="download" />
-                Download
-              </semantic.Menu.Item>
-              <semantic.Menu.Item as="a" href={aislerUrl}>
-                <img src="/images/aisler.png" />
-                <semantic.Label floating={!matches}>
-                  <semantic.Flag name="de" />
-                </semantic.Label>
-              </semantic.Menu.Item>
-              <semantic.Menu.Item as="a" href={pcbwayUrl}>
-                <img src="/images/pcbway.png" />
-                <semantic.Label floating={!matches}>
-                  <semantic.Flag name="cn" />
-                </semantic.Label>
-              </semantic.Menu.Item>
-              <semantic.Menu.Item
-                as="a"
-                href={royalCircuitsUrl}
-                onClick={() => {
-                  if (!this.state.downloaded) {
-                    window.open(zipPath)
-                  }
-                }}
-              >
-                <img src="/images/royal_circuits.png" />
-                <semantic.Label floating={!matches}>
-                  <semantic.Flag name="us" />
-                </semantic.Label>
-              </semantic.Menu.Item>
+          <div className="PcbMenu">
+            <semantic.Menu stackable={matches} compact borderless>
+              <div className="PcbMenu__container">
+                <div className="PcbMenu__group PcbMenu__download">
+                  <semantic.Menu.Item as="a" href={zipPath}>
+                    <semantic.Icon size="big" name="download" />
+                    Download Gerber Files
+                  </semantic.Menu.Item>
+                </div>
+
+                <div className="PcbMenu__group">
+                  <div className="PcbMenu__sub-title">
+                    <semantic.Menu.Item>
+                      <h4>Order PCBs:</h4>
+                    </semantic.Menu.Item>
+                  </div>
+
+                  <div className="PcbMenu__links-container">
+                    <semantic.Menu.Item
+                      as="a"
+                      href={aislerUrl}
+                      className="PcbMenu__link"
+                    >
+                      <img src="/images/aisler.png" />
+                      <semantic.Label floating={!matches}>
+                        <semantic.Flag name="de" />
+                      </semantic.Label>
+                    </semantic.Menu.Item>
+
+                    <semantic.Menu.Item
+                      as="a"
+                      href={pcbwayUrl}
+                      className="PcbMenu__link"
+                    >
+                      <img src="/images/pcbway.png" />
+                      <semantic.Label floating={!matches}>
+                        <semantic.Flag name="cn" />
+                      </semantic.Label>
+                    </semantic.Menu.Item>
+
+                    <semantic.Menu.Item
+                      as="a"
+                      href={royalCircuitsUrl}
+                      className="PcbMenu__link"
+                      onClick={() => {
+                        if (!this.state.downloaded) {
+                          window.open(zipPath)
+                        }
+                      }}
+                    >
+                      <img src="/images/royal_circuits.png" />
+                      <semantic.Label floating={!matches}>
+                        <semantic.Flag name="us" />
+                      </semantic.Label>
+                    </semantic.Menu.Item>
+                  </div>
+                </div>
+
+                <div className="PcbMenu__group PcbMenu__compare">
+                  <div className="PcbMenu__sub-title">
+                    <semantic.Menu.Item>
+                      <h4>Compare PCB Prices:</h4>
+                    </semantic.Menu.Item>
+                  </div>
+                  <div className="PcbMenu__links-container">
+                    <semantic.Menu.Item as="a" href={pcbShopperUrl}>
+                      <img src="/images/pcbshopper.png" />
+                      <semantic.Label floating={!matches} />
+                    </semantic.Menu.Item>
+                  </div>
+                </div>
+              </div>
             </semantic.Menu>
           </div>
         )}
