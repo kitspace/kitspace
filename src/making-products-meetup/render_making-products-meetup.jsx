4cd1de0cccab25f324550e13a57d17180319e7bf
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-02-01 19:12:02 +0000

    Add making-products-meetup page

---
+++ src/making-products-meetup/render_making-products-meetup.jsx
@@ -0,0 +1,5 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const MakingProductsMeetup = require('./making-products-meetup')
+
+ReactDOM.render(<MakingProductsMeetup />, document.getElementById('content'))
