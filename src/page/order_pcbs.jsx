6bdff5348e80bc4e110e08b67337e69347791bf2
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-06-06 22:36:11 +0100

    Removes Royal Circuits
    fixes #267

--- src/page/order_pcbs.jsx
+++ src/page/order_pcbs.jsx
@@ -8,7 +8,6 @@ const {zipPath, folder, width, height, layers} = require('../zip-info.json')
 const zipUrl = `https://kitspace.org/${folder}/${zipPath}`
 const aislerUrl = `https://aisler.net/p/new?url=${zipUrl}&ref=kitspace`
 const pcbwayUrl = `https://www.pcbway.com/QuickOrderOnline.aspx?fileurl=${zipUrl}&from=kitspace`
-const royalCircuitsUrl = 'https://www.royalcircuits.com/'
 const oshparkUrl = `https://oshpark.com/import?url=${zipUrl}`
 const pcbShopperUrl = `https://pcbshopper.com/?Width=${width}&Height=${height}&Units=mm&Layers=${layers}&Quantity=1&GetPrices`
 
@@ -58,17 +57,6 @@ let OrderPcbs = React.createClass({
                       <img src="/images/pcbway.png" />
                       <semantic.Label floating={!matches}>
                         <semantic.Flag name="cn" />
-                      </semantic.Label>
-                    </semantic.Menu.Item>
-
-                    <semantic.Menu.Item
-                      as="a"
-                      href={royalCircuitsUrl}
-                      className="PcbMenu__link"
-                    >
-                      <img src="/images/royal_circuits.png" />
-                      <semantic.Label floating={!matches}>
-                        <semantic.Flag name="us" />
                       </semantic.Label>
                     </semantic.Menu.Item>
 
