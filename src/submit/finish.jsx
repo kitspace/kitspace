8803f22c244f83246e9a47e21731f80838dd85b4
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-06-14 18:20:54 +0100

    Move /submit terms and conditions to final step

--- src/submit/finish.jsx
+++ src/submit/finish.jsx
@@ -5,6 +5,16 @@ const {Container} = require('semantic-ui-react')
 const Steps = require('./steps')
 const UrlSubmit = require('./url_submit')
 
+const terms_and_conditions = `
+
+### Terms and Conditions
+
+1. We (Kitspace developers) do not claim any ownership over your work, it remains yours.
+2. By submitting your project you give us permission to host copies of your files for other people to download.
+3. If you change your mind, you can remove your project any time by removing the public git repository, sending a pull-request to remove it from [\`boards.txt\`](https://github.com/kitspace/kitspace/blob/master/boards.txt) or notifying [@kasbah](https://github.com/kasbah) in some other way.
+
+`
+
 const Step4 = React.createClass({
   render() {
     const board = this.props.board
@@ -27,6 +37,7 @@ const Step4 = React.createClass({
             style={{marginTop: 30, display: 'flex', justifyContent: 'center'}}
           >
             <img style={{height: 200}} src="/images/fireworks.png" />
+            <Markdown className="instructions" source={terms_and_conditions} />
           </div>
         </Container>
       </div>
