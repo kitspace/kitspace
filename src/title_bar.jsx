9dd32d8238ae4aaee280ae6aeb2a1ec81b931c3b
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-06-14 21:47:21 +0100

    Use TitleBar redesign from kitspace-using-gitea

--- src/title_bar.jsx
+++ src/title_bar.jsx
@@ -1,111 +1,117 @@
 const React = require('react')
 const semantic = require('semantic-ui-react')
-const ReactResponsive = require('react-responsive')
-
-const mediaQueries = require('./media_queries')
 
 function TitleBar(props) {
+  const isSubmitRoute = RegExp('^/projects/new').test(props.route)
   const isProjectRoute =
-    props.route === '/' || RegExp('^/boards/').test(props.route)
-  const isSubmitRoute = RegExp('^/submit/').test(props.route)
+    isSubmitRoute ||
+    props.route === '/' ||
+    RegExp('^/projects/').test(props.route)
   return (
-    <ReactResponsive query={mediaQueries.mobile}>
-      {matches => (
-        <div className="titleBar">
-          <div className="logoContainer">
-            <semantic.Menu inverted pointing secondary stackable={matches}>
-              <a href="/">
-                <semantic.Image className="logoImg" src="/images/logo.svg" />
-              </a>
-              <semantic.Menu.Item
-                as="a"
-                href="/"
-                active={isProjectRoute || isSubmitRoute}
-              >
-                {'Projects'}
-              </semantic.Menu.Item>
-              <semantic.Menu.Item
-                as="a"
-                href="/bom-builder"
-                active={props.route === '/bom-builder/'}
-              >
-                {'BOM Builder'}
-              </semantic.Menu.Item>
-              <semantic.Menu.Item
-                as="a"
-                href="/1-click-bom"
-                active={props.route === '/1-click-bom/'}
-              >
-                {'1-click BOM'}
-              </semantic.Menu.Item>
-            </semantic.Menu>
-          </div>
-          <div className="rightButtonsContainer">
-            <semantic.Menu basic inverted stackable={matches}>
-              {isSubmitRoute ? null : (
-                <semantic.Menu.Item>
-                  <semantic.Button
-                    icon
-                    labelPosition="left"
-                    color="green"
-                    href="/submit"
-                  >
-                    <semantic.Icon name="plus" />
-                    Add a project
-                  </semantic.Button>
-                </semantic.Menu.Item>
-              )}
-              <semantic.Popup
-                trigger={
-                  <semantic.Menu.Item className="contact-button">
-                    <semantic.Button labelPosition="right" icon color="blue">
-                      <semantic.Icon inverted name="comments" />
-                      Make contact
-                    </semantic.Button>
-                  </semantic.Menu.Item>
-                }
-                on="click"
-                position="bottom right"
-                color="blue"
-              >
-                <semantic.Menu color="blue" secondary vertical>
-                  <semantic.Menu.Item
-                    as="a"
-                    href="https://riot.im/app/#/room/#kitspace:matrix.org"
-                  >
-                    <semantic.Icon name="chat" />
-                    Chat
-                  </semantic.Menu.Item>
-                  <semantic.Menu.Item as="a" href="/newsletter/">
-                    <semantic.Icon name="envelope" />
-                    Email & Newsletter
-                  </semantic.Menu.Item>
-                  <semantic.Menu.Item
-                    as="a"
-                    href="https://twitter.com/kitspaceorg"
-                  >
-                    <semantic.Icon name="twitter" />
-                    Twitter
-                  </semantic.Menu.Item>
-                  <semantic.Menu.Item as="a" href="https://github.com/kitspace">
-                    <semantic.Icon name="github" />
-                    GitHub
-                  </semantic.Menu.Item>
-                  <semantic.Menu.Item
-                    as="a"
-                    href="https://opencollective.com/kitspace"
-                  >
-                    <semantic.Icon name="heart" />
-                    Donate
-                  </semantic.Menu.Item>
-                </semantic.Menu>
-              </semantic.Popup>
-            </semantic.Menu>
-          </div>
-        </div>
-      )}
-    </ReactResponsive>
+    <div className="titleBar">
+      <div className="bigSiteMenu">
+        <semantic.Menu inverted pointing secondary>
+          <a href="/">
+            <semantic.Image className="logoImg" src="/images/logo.svg" />
+          </a>
+          <SiteMenuItems route={props.route} isProjectRoute={isProjectRoute} />
+        </semantic.Menu>
+      </div>
+      <div className="bigSocialMenu">
+        <semantic.Menu inverted pointing secondary>
+          <SocialMenuItems isSubmitRoute={isSubmitRoute} />
+        </semantic.Menu>
+      </div>
+      <div className="smallMenu">
+        <a href="/">
+          <semantic.Image className="logoImg" src="/images/logo.svg" />
+        </a>
+        <semantic.Popup
+          trigger={
+            <semantic.Button icon size="large" basic inverted>
+              <semantic.Icon inverted name="bars" />
+            </semantic.Button>
+          }
+          on="click"
+          position="bottom right"
+          inverted
+          basic
+        >
+          <semantic.Menu inverted vertical>
+            <SiteMenuItems
+              route={props.route}
+              isProjectRoute={isProjectRoute}
+            />
+            <SocialMenuItems isSubmitRoute={isSubmitRoute} />
+          </semantic.Menu>
+        </semantic.Popup>
+      </div>
+    </div>
   )
 }
+
+function SiteMenuItems(props) {
+  return (
+    <>
+      <semantic.Menu.Item as="a" href="/" active={props.isProjectRoute}>
+        {'Projects'}
+      </semantic.Menu.Item>
+      <semantic.Menu.Item
+        as="a"
+        href="/bom-builder"
+        active={props.route === '/bom-builder/'}
+      >
+        {'BOM Builder'}
+      </semantic.Menu.Item>
+      <semantic.Menu.Item
+        as="a"
+        href="/1-click-bom"
+        active={props.route === '/1-click-bom/'}
+      >
+        {'1-click BOM'}
+      </semantic.Menu.Item>
+    </>
+  )
+}
+
+function SocialMenuItems(props) {
+  return (
+    <>
+      <semantic.Menu.Item
+        as="a"
+        href="https://riot.im/app/#/room/#kitspace:matrix.org"
+      >
+        <semantic.Icon name="chat" />
+        Chat
+      </semantic.Menu.Item>
+      <semantic.Menu.Item as="a" href="https://twitter.com/kitspaceorg">
+        <semantic.Icon name="twitter" />
+        Twitter
+      </semantic.Menu.Item>
+      <semantic.Menu.Item as="a" href="https://github.com/kitspace">
+        <semantic.Icon name="github" />
+        GitHub
+      </semantic.Menu.Item>
+      <semantic.Menu.Item as="a" href="https://opencollective.com/kitspace">
+        <semantic.Icon name="heart" />
+        Donate
+      </semantic.Menu.Item>
+      {props.isSubmitRoute ? null : (
+        <semantic.Menu.Item>
+          <semantic.Button
+            icon
+            labelPosition="left"
+            color="green"
+            href="/projects/new"
+          >
+            <semantic.Icon name="plus" />
+            Add a project
+          </semantic.Button>
+        </semantic.Menu.Item>
+      )}
+    </>
+  )
+}
+
 
 module.exports = TitleBar
