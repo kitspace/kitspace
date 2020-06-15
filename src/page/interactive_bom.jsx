9a3eccb2cb345ab56eddc268bbf6cdd1b3f307e3
Ian Ross <ian@skybluetrades.net>
2020-06-15 20:00:58 +0200

    Roughest of the rough first pass
     - No customisation of look of IBOM pages
     - Fairly messy additions to build system
    
    But it does work: boards that have a KiCad PCB file in them have an
    interactive BOM built and linked from the board page.
    
    (There are also some changes in this branch to help with running
    Travis on forks of the main kitspace repo.)

---
+++ src/page/interactive_bom.jsx
@@ -0,0 +1,35 @@
+const React = require('react')
+const semantic = require('semantic-ui-react')
+const ReactResponsive = require('react-responsive')
+
+const {folder} = require('../zip-info.json')
+const ibomUrl = `https://kitspace.org/${folder}/interactive_bom.html`
+
+let InteractiveBOM = React.createClass({
+  render() {
+    return (
+      <ReactResponsive
+        query={
+        '(max-width: 920px)' // XXX if you change this change it in the .scss too!
+        }
+      >
+        {matches => (
+          <div className="InteractiveBOMMenu">
+            <semantic.Menu stackable={matches} compact borderless>
+              <div className="InteractiveBOMMenu__container">
+                <div className="InteractiveBOMMenu__link">
+                  <semantic.Menu.Item as="a" href="interactive_bom.html">
+                    <semantic.Icon size="big" name="columns" />
+                    Interactive BOM
+                  </semantic.Menu.Item>
+                </div>
+              </div>
+            </semantic.Menu>
+          </div>
+        )}
+      </ReactResponsive>
+    )
+  }
+})
+
+module.exports = InteractiveBOM
--- src/page/page.jsx
+++ src/page/page.jsx
@@ -9,6 +9,7 @@ const InfoBar = require('./info_bar')
 const TitleBar = require('../title_bar')
 const FadeImage = require('../fade_image')
 const BuyParts = require('../buy_parts/buy_parts')
+const InteractiveBOM = require('./interactive_bom')
 const Readme = require('../readme')
 const semantic = require('semantic-ui-react')
 
@@ -61,6 +62,7 @@ const Page = createClass({
               <FadeImage src="images/bottom.svg" />
             </BoardShowcase>
             <OrderPcbs />
+            {info.has_interactive_bom && <InteractiveBOM />}
             <BuyParts lines={info.bom.lines} parts={info.bom.parts} />
             <div className="readme-container">
               <Readme />
