a24fb253bc367db3814c722a03c5eee1ed278ad9
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-08-25 19:48:01 +0100

    Change ibom button

---
+++ src/page/ibom_icon.jsx
@@ -0,0 +1,50 @@
+const React = require('react')
+
+function IBomIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 8.47 8.47"
+      height="18"
+      style={{marginRight: 10}}
+    >
+      <rect
+        transform="translate(0 -288.53)"
+        fill="#9f5"
+        width="7.94"
+        height="7.94"
+        x=".27"
+        y="288.8"
+        ry="1.17"
+      />
+      <g transform="translate(0 -288.53)">
+        <rect
+          stroke-linejoin="round"
+          stroke-width=".4"
+          stroke="#000"
+          fill="none"
+          ry="1.17"
+          y="288.8"
+          x=".27"
+          height="7.94"
+          width="7.94"
+        />
+        <path
+          stroke-width=".4"
+          stroke="#000"
+          fill="#4d4d4d"
+          d="M1.06 290.12H3.7m-2.64 1.33H3.7m-2.64 1.32H3.7m-2.64 1.3H3.7m-2.64 1.33H3.7"
+        />
+        <path
+          stroke-width=".3"
+          stroke="#000"
+          fill="none"
+          d="M4.37 288.8v7.94m0-4.11h3.96"
+        />
+        <path d="M5.401 289.65h1.608v.45H5.997v.43h.952v.45h-.952v.98H5.4zM6.247 294.264q.14 0 .213-.062.073-.062.073-.183 0-.119-.073-.18-.073-.064-.213-.064h-.33v.489zm.02 1.01q.18 0 .27-.075.09-.076.09-.23 0-.15-.09-.224-.09-.076-.27-.076h-.35v.605zm.554-.83q.192.055.297.205.105.15.105.369 0 .334-.226.498-.226.164-.687.164h-.989v-2.31h.895q.481 0 .696.145.217.145.217.465 0 .17-.08.288-.078.118-.228.175z" />
+      </g>
+    </svg>
+  )
+}
+
+module.exports = IBomIcon
--- src/page/interactive_bom_button.jsx
+++ src/page/interactive_bom_button.jsx
@@ -1,23 +1,21 @@
 const React = require('react')
-const semantic = require('semantic-ui-react')
+const {Button, Header} = require('semantic-ui-react')
+const IBomIcon = require('./ibom_icon')
 
 const info = require('../info.json')
 
 function InteractiveBOMButton() {
   const ibomUrl = `/interactive_bom?${info.id}`
   return (
-    <div className="InteractiveBOMButton">
-      <semantic.Menu compact borderless>
-        <div className="InteractiveBOMButton__container">
-          <div className="InteractiveBOMButton__link">
-            <semantic.Menu.Item as="a" href={ibomUrl}>
-              <semantic.Icon size="big" name="microchip" />
-              Assembly Assistant
-            </semantic.Menu.Item>
-          </div>
-        </div>
-      </semantic.Menu>
-    </div>
+    <Button basic as="a" href={ibomUrl}>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <Header as="h4">
+          <IBomIcon />
+          Assembly Guide
+        </Header>
+        <div>Interactive HTML BOM</div>
+      </div>
+    </Button>
   )
 }
 
