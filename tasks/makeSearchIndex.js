df9db28426f1d449f08d2de4837959f572c14ad9
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-10-08 16:54:02 +0100

    Fix cached_build being a string

--- tasks/makeSearchIndex.js
+++ tasks/makeSearchIndex.js
@@ -5,7 +5,7 @@ const {parseProjects} = require('./utils/parseProjects')
 if (require.main !== module) {
   module.exports = function(config) {
     const targets = ['build/.temp/search_index.json']
-    const boards = parseProjects(config, process.env.CACHED_BUILD)
+    const boards = parseProjects(config, process.env.CACHED_BUILD === "cached")
     const boardsInfoPaths = boards.map(
       b => `build/.temp/boards/${b.id}/info.json`
     )
