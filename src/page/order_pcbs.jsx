34677301b15d8c756bd8a7c28025caf01fe04580
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-11-19 22:10:18 +0000

    Add jlcpcb link

--- src/page/order_pcbs.jsx
+++ src/page/order_pcbs.jsx
@@ -1,7 +1,6 @@
 const React = require('react')
 const createClass = require('create-react-class')
 const semantic = require('semantic-ui-react')
-const ReactResponsive = require('react-responsive')
 
 const mediaQueries = require('../media_queries')
 const {zipPath, folder, width, height, layers} = require('../zip-info.json')
@@ -11,87 +10,61 @@ const aislerUrl = `https://aisler.net/p/new?url=${zipUrl}&ref=kitspace`
 const pcbwayUrl = `https://www.pcbway.com/QuickOrderOnline.aspx?fileurl=${zipUrl}&from=kitspace`
 const oshparkUrl = `https://oshpark.com/import?url=${zipUrl}`
 const pcbShopperUrl = `https://pcbshopper.com/?Width=${width}&Height=${height}&Units=mm&Layers=${layers}&Quantity=1&GetPrices`
+const jlcpcbUrl = `https://cart.jlcpcb.com/quote?fileurl=${zipUrl}&from=kitspace`
 
 let OrderPcbs = createClass({
   render() {
     return (
-      <ReactResponsive
-        query={
-          '(max-width: 920px)' // XXX if you change this change it in the .scss too!
-        }
-      >
-        {matches => (
-          <div className="PcbMenu">
-            <semantic.Menu stackable={matches} compact borderless>
-              <div className="PcbMenu__container">
-                <div className="PcbMenu__group PcbMenu__download">
-                  <semantic.Menu.Item as="a" href={zipPath}>
-                    <semantic.Icon size="big" name="download" />
-                    Download Gerber Files
-                  </semantic.Menu.Item>
-                </div>
+      <div className="PcbMenu">
+        <div className="PcbMenu__group PcbMenu__download">
+          <a href={zipPath} className="PcbMenu__link">
+            <semantic.Icon name="download" />
+            Download Gerbers
+          </a>
+        </div>
 
-                <div className="PcbMenu__group">
-                  <div className="PcbMenu__sub-title">
-                    <semantic.Menu.Item>
-                      <h4>Order PCBs:</h4>
-                    </semantic.Menu.Item>
-                  </div>
-
-                  <div className="PcbMenu__links-container">
-                    <semantic.Menu.Item
-                      as="a"
-                      href={aislerUrl}
-                      className="PcbMenu__link"
-                    >
-                      <img src="/images/aisler.png" />
-                      <semantic.Label floating={!matches}>
-                        <semantic.Flag name="de" />
-                      </semantic.Label>
-                    </semantic.Menu.Item>
-
-                    <semantic.Menu.Item
-                      as="a"
-                      href={pcbwayUrl}
-                      className="PcbMenu__link"
-                    >
-                      <img src="/images/pcbway.png" />
-                      <semantic.Label floating={!matches}>
-                        <semantic.Flag name="cn" />
-                      </semantic.Label>
-                    </semantic.Menu.Item>
-
-                    <semantic.Menu.Item
-                      as="a"
-                      href={oshparkUrl}
-                      className="PcbMenu__link"
-                    >
-                      <img src="/images/oshpark.png" />
-                      <semantic.Label floating={!matches}>
-                        <semantic.Flag name="us" />
-                      </semantic.Label>
-                    </semantic.Menu.Item>
-                  </div>
-                </div>
-
-                <div className="PcbMenu__group PcbMenu__compare">
-                  <div className="PcbMenu__sub-title">
-                    <semantic.Menu.Item>
-                      <h4>Compare PCB Prices:</h4>
-                    </semantic.Menu.Item>
-                  </div>
-                  <div className="PcbMenu__links-container">
-                    <semantic.Menu.Item as="a" href={pcbShopperUrl}>
-                      <img src="/images/pcbshopper.png" />
-                      <semantic.Label floating={!matches} />
-                    </semantic.Menu.Item>
-                  </div>
-                </div>
-              </div>
-            </semantic.Menu>
+        <div
+          className="PcbMenu__group"
+          style={{flexGrow: 1, justifyContent: 'space-around'}}
+        >
+          <div className="PcbMenu__sub-title">
+            <h4>Order PCBs:</h4>
           </div>
-        )}
-      </ReactResponsive>
+
+          <div className="PcbMenu__links-container">
+            <a href={aislerUrl} className="PcbMenu__link">
+              <img src="/images/aisler.png" />
+              <semantic.Flag name="de" />
+            </a>
+
+            <a href={pcbwayUrl} className="PcbMenu__link">
+              <img src="/images/pcbway.png" />
+              <semantic.Flag name="cn" />
+            </a>
+
+            <a href={jlcpcbUrl} className="PcbMenu__link">
+              <img src="/images/jlcpcb.png" />
+              <semantic.Flag name="cn" />
+            </a>
+
+            <a href={oshparkUrl} className="PcbMenu__link">
+              <img src="/images/oshpark.png" />
+              <semantic.Flag name="us" />
+            </a>
+          </div>
+        </div>
+
+        <div className="PcbMenu__group PcbMenu__compare">
+          <div className="PcbMenu__sub-title">
+            <h4>Compare PCB Prices:</h4>
+          </div>
+          <div className="PcbMenu__links-container">
+            <a href={pcbShopperUrl} className="PcbMenu__link">
+              <img src="/images/pcbshopper.png" />
+            </a>
+          </div>
+        </div>
+      </div>
     )
   }
 })
