0d517fce9ad44f2989e37f4218336a7be516f101
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2021-03-12 20:12:41 +0000

    Add plausible events for extension install

--- src/install_extension.jsx
+++ src/install_extension.jsx
@@ -4,16 +4,19 @@ function installExtension() {
   let onClick
   if (/Chrome/.test(version)) {
     onClick = () => {
+      window.plausible != null && window.plausible('Install Extension')
       window.open(
         'https://chrome.google.com/webstore/detail/kitspace-1-click-bom/mflpmlediakefinapghmabapjeippfdi'
       )
     }
   } else if (/Firefox/.test(version)) {
     onClick = () => {
+      window.plausible != null && window.plausible('Install Extension')
       window.open('https://addons.mozilla.org/en-US/firefox/addon/1clickbom')
     }
   } else {
     onClick = () => {
+      window.plausible != null && window.plausible('Install Extension')
       window.open('/1-click-bom', '_self')
     }
   }
